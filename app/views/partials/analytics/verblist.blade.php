66292d8cfd16d55ff7e5e978eff8bfcdbfca1fb7
Dave Tosh <davidgtosh@gmail.com>
2014-02-06 18:05:36 -0500

    Remove old files

--- app/views/partials/analytics/verblist.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="panel panel-default">
-	<div class="panel-heading">
-		Popular verbs
-	</div>
-	<div class="panel-body">
-		@include('partials.analytics.data.verblist', array('verbs' => $data['result'], 'total' => $data['total'], 'lrs' => $lrs))
-	</div>
-</div>
