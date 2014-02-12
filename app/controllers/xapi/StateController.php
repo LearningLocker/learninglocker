89963d22c878279cbbab7de2bacbc939ce07c426
James Mullaney <james@ht2.co.uk>
2014-02-12 10:44:46 +0000

    Use array_column to return list of stateIds for StateController. Included array_column fallback in composer

--- app/controllers/xapi/StateController.php
+++ app/controllers/xapi/StateController.php
@@ -5,115 +5,117 @@ use Locker\Repository\Document\DocumentType as DocumentType;
 
 class StateController extends DocumentController {
 
-	/**
-	 * Construct
-	 *
-	 * @param DocumentRepository $document
-	 */
-	public function __construct(Document $document){
+  /**
+   * Construct
+   *
+   * @param DocumentRepository $document
+   */
+  public function __construct(Document $document){
 
-		parent::__construct($document);
+    parent::__construct($document);
 
-		$this->document_type = DocumentType::STATE;
+    $this->document_type = DocumentType::STATE;
 
-	}
-
-
-	/**
-	 * Return a list of stateId's based on activityId and actor match.
-	 *
-	 * @todo add in actor details
-	 *
-	 * @return Response
-	 */
-	public function index(){
-
-		$data = $this->checkParams(array(
-			'activityId' => 'string',
-			'agent'      => array('string', 'json')
-		), array(
-			'registration' => 'string',
-			'since'        => 'timestamp'
-		), $this->params );
-
-		$documents = $this->document->all( $this->lrs->_id, $this->document_type, $data['activityId'], $data['agent'] );
-
-		return \Response::json( $documents->toArray() );
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store(){
-
-		$state = $this->checkParams( 
-			array(
-				'activityId' => 'string',
-				'agent'      => array('string', 'json'),
-				'stateId'    => 'string',
-				'content'    => ''
-			),
-			array(
-				'registration' => 'string'
-			),
-			$this->params
-		);
+  }
 
 
-		$store = $this->document->store( $this->lrs->_id, $state, $this->document_type );
+  /**
+   * Return a list of stateId's based on activityId and actor match.
+   *
+   * @todo add in actor details
+   *
+   * @return Response
+   */
+  public function index(){
 
-		if( $store ){
-			return \Response::json( array( 'ok', 204 ) );
-		}
+    $data = $this->checkParams(array(
+      'activityId' => 'string',
+      'agent'      => array('string', 'json')
+    ), array(
+      'registration' => 'string',
+      'since'        => 'timestamp'
+    ), $this->params );
 
-		return \Response::json( array( 'error', 400 ) );
+    $documents = $this->document->all( $this->lrs->_id, $this->document_type, $data['activityId'], $data['agent'] );
+    
+    //return array of only the stateId values for each document
+    $stateIds = array_column($documents->toArray(), 'stateId');
+    return \Response::json( $stateIds );
+  }
 
-	}
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @return Response
+   */
+  public function store(){
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  string     $activityId
-	 * @param  json       $agent
-	 * @param  string     $stateId
-	 * @return Response
-	 */
-	public function show( $activityId, $agent, $stateId ){
+    $state = $this->checkParams( 
+      array(
+        'activityId' => 'string',
+        'agent'      => array('string', 'json'),
+        'stateId'    => 'string',
+        'content'    => ''
+      ),
+      array(
+        'registration' => 'string'
+      ),
+      $this->params
+    );
 
-		$data = $this->checkParams(array(
-			'activityId' => 'string',
-			'stateId'	 => 'string',
-			'agent'      => array('string', 'json')
-		), array(), $this->params );
 
-		$documents = $this->document->all( $this->lrs->_id, $this->document_type, $data['activityId'], $data['agent'], $data['stateId'] );
+    $store = $this->document->store( $this->lrs->_id, $state, $this->document_type );
 
-		return \Response::json( $document->toArray() );
+    if( $store ){
+      return \Response::json( array( 'ok', 204 ) );
+    }
 
-	}
+    return \Response::json( array( 'error', 400 ) );
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+  }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+  /**
+   * Display the specified resource.
+   *
+   * @param  string     $activityId
+   * @param  json       $agent
+   * @param  string     $stateId
+   * @return Response
+   */
+  public function show( $activityId, $agent, $stateId ){
+
+    $data = $this->checkParams(array(
+      'activityId' => 'string',
+      'stateId'  => 'string',
+      'agent'      => array('string', 'json')
+    ), array(), $this->params );
+
+    $documents = $this->document->all( $this->lrs->_id, $this->document_type, $data['activityId'], $data['agent'], $data['stateId'] );
+
+    return \Response::json( $document->toArray() );
+
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function update($id)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function destroy($id)
+  {
+    //
+  }
 
 }
