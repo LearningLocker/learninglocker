83690ff60468ec61732b04f8b219fedd0e0e5e3b
Dave Tosh <davidgtosh@gmail.com>
2014-02-12 12:57:03 -0500

    tabs -> spaces

--- app/controllers/AnalyticsController.php
+++ app/controllers/AnalyticsController.php
@@ -6,58 +6,58 @@ use Locker\Data\Analytics\AnalyticsInterface;
 
 class AnalyticsController extends BaseController {
 
-	/**
-	* Lrs 
-	*/
-	protected $lrs;
+  /**
+  * Lrs 
+  */
+  protected $lrs;
 
-	/**
-	* Analytics
-	*/
-	protected $analytics;
+  /**
+  * Analytics
+  */
+  protected $analytics;
 
-	/**
-	 * Statements
-	 **/
-	protected $statements;
+  /**
+   * Statements
+   **/
+  protected $statements;
 
 
-	/**
-	 * Construct
-	 *
-	 * @param Locker\Repository\Lrs\LrsRepository
-	 * @param Locker\Data\AnalyticsInterface
-	 * @param Locker\Repository\StatementRepository
-	 *
-	 */
-	public function __construct(Lrs $lrs, AnalyticsInterface $analytics, Statement $statement){
+  /**
+   * Construct
+   *
+   * @param Locker\Repository\Lrs\LrsRepository
+   * @param Locker\Data\AnalyticsInterface
+   * @param Locker\Repository\StatementRepository
+   *
+   */
+  public function __construct(Lrs $lrs, AnalyticsInterface $analytics, Statement $statement){
 
-		$this->lrs       = $lrs;
-		$this->analytics = $analytics;
-		$this->statement = $statement;
+    $this->lrs       = $lrs;
+    $this->analytics = $analytics;
+    $this->statement = $statement;
 
-		$this->beforeFilter('auth');
+    $this->beforeFilter('auth');
 
-	}
+  }
 
-	/**
-	 * Display the analytics view.
-	 *
-	 * @return View
-	 */
-	public function index( $id, $segment='verbs' ){
+  /**
+   * Display the analytics view.
+   *
+   * @return View
+   */
+  public function index( $id, $segment='verbs' ){
 
-		$lrs 	  = $this->lrs->find( $id );
-		$lrs_list = $this->lrs->all();
-		$this->analytics->getAnalytics( $id, $segment );
-		return View::make('partials.analytics.index', array('lrs'           => $lrs,
-														  	'data'		    => $this->analytics->results,
-														  	'line_graph_data' => $this->analytics->data,
-														  	'selected'	    => $segment,
-														  	'analytics_nav' => true,
-														  	'list'          => $lrs_list));
+    $lrs    = $this->lrs->find( $id );
+    $lrs_list = $this->lrs->all();
+    $this->analytics->getAnalytics( $id, $segment );
+    return View::make('partials.analytics.index', array('lrs'           => $lrs,
+                                'data'        => $this->analytics->results,
+                                'line_graph_data' => $this->analytics->data,
+                                'selected'      => $segment,
+                                'analytics_nav' => true,
+                                'list'          => $lrs_list));
 
-	}
+  }
 
 
 
--- app/controllers/StatementController.php
+++ app/controllers/StatementController.php
@@ -5,97 +5,97 @@ use Locker\Repository\Lrs\LrsRepository as Lrs;
 
 class StatementController extends BaseController {
 
-	/**
-	* Statement
-	*/
-	protected $statement;
+  /**
+  * Statement
+  */
+  protected $statement;
 
-	/**
-	* Lrs 
-	*/
-	protected $lrs;
+  /**
+  * Lrs 
+  */
+  protected $lrs;
 
-	/**
-	 * Construct
-	 *
-	 * @param StatementRepository $statement
-	 */
-	public function __construct( Statement $statement, Lrs $lrs ){
+  /**
+   * Construct
+   *
+   * @param StatementRepository $statement
+   */
+  public function __construct( Statement $statement, Lrs $lrs ){
 
-		$this->statement = $statement;
-		$this->lrs 	     = $lrs;
+    $this->statement = $statement;
+    $this->lrs       = $lrs;
 
-		$this->beforeFilter('auth');
-		$this->beforeFilter('csrf', array('on' => 'store'));
-		$this->beforeFilter('@checkCanSubmit', array('only' => 'store'));
+    $this->beforeFilter('auth');
+    $this->beforeFilter('csrf', array('on' => 'store'));
+    $this->beforeFilter('@checkCanSubmit', array('only' => 'store'));
 
-	}
+  }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return View
-	 */
-	public function create( $id ){
-		$lrs = $this->lrs->find( $id );
-		return View::make('partials.statements.create', array('lrs'           => $lrs,
-															  'statement_nav' => true));
-	}
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return View
+   */
+  public function create( $id ){
+    $lrs = $this->lrs->find( $id );
+    return View::make('partials.statements.create', array('lrs'           => $lrs,
+                                'statement_nav' => true));
+  }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * This is ony used via the manual statement generator on the 
-	 * site. Look in /api/statements for incoming statements.
-	 *
-	 * @return Response
-	 */
-	public function store(){
+  /**
+   * Store a newly created resource in storage.
+   *
+   * This is ony used via the manual statement generator on the 
+   * site. Look in /api/statements for incoming statements.
+   *
+   * @return Response
+   */
+  public function store(){
 
-		$input = Input::all();
+    $input = Input::all();
 
-		$lrs = $this->lrs->find( $input['lrs'] );
+    $lrs = $this->lrs->find( $input['lrs'] );
 
-		//remove lrs and _token from Input
-		unset( $input['lrs'] );
-		unset( $input['_token'] );
+    //remove lrs and _token from Input
+    unset( $input['lrs'] );
+    unset( $input['_token'] );
 
-		//add mailto to actor mbox
-		$input['actor']['mbox'] = 'mailto:' . $input['actor']['mbox'];
+    //add mailto to actor mbox
+    $input['actor']['mbox'] = 'mailto:' . $input['actor']['mbox'];
 
-		// Save the statement
-		$s = $this->statement->create( $input, $lrs );
+    // Save the statement
+    $s = $this->statement->create( $input, $lrs );
 
-		if($s){
-			return Redirect::back()->with('success', Lang::get('statement.added'));
-		}
+    if($s){
+      return Redirect::back()->with('success', Lang::get('statement.added'));
+    }
 
-		return Redirect::back()
-				->withInput()
-				->withErrors($s->errors());
+    return Redirect::back()
+        ->withInput()
+        ->withErrors($s->errors());
 
-	}
+  }
 
-	/**
-	 * Can current user submit statements to this LRS?
-	 **/
-	public function checkCanSubmit( $route, $request ){
+  /**
+   * Can current user submit statements to this LRS?
+   **/
+  public function checkCanSubmit( $route, $request ){
 
-		$user      = \Auth::user();
-		$lrs       = $this->lrs->find( Input::get('lrs') );
-		$get_users = array();
+    $user      = \Auth::user();
+    $lrs       = $this->lrs->find( Input::get('lrs') );
+    $get_users = array();
 
-		if( $lrs ){
-			foreach( $lrs->users as $u ){
-				$get_users[] = $u['_id'];
-			}
-		}
+    if( $lrs ){
+      foreach( $lrs->users as $u ){
+        $get_users[] = $u['_id'];
+      }
+    }
 
-		//check current user is in the list of allowed users, or is super admin
-		if( !in_array($user->_id, $get_users) && $user->role != 'super' ){
-			return Redirect::to('/');
-		}
+    //check current user is in the list of allowed users, or is super admin
+    if( !in_array($user->_id, $get_users) && $user->role != 'super' ){
+      return Redirect::to('/');
+    }
 
-	}
+  }
 
 }
