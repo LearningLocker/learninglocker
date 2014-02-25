bcfad12594cf291a773bad5b7f48ca40a883e9b3
Dave Tosh <davidgtosh@gmail.com>
2014-02-25 13:43:15 -0500

    Temp hotfix for verb issue if verb display is a url

--- app/views/partials/analytics/data/popular_verbs.blade.php
+++ app/views/partials/analytics/data/popular_verbs.blade.php
@@ -18,26 +18,32 @@
 @foreach( $verbs as $v )
 
 <?php 
-  $verb            = isset($v['verb']['0']) ? $v['verb']['0'] : 'undefined';
+  $verb            = isset($v['verb']['0']) ? reset($v['verb']['0']) : 'undefined';
   $percent         = round((($v['count'] / $subtotal) * 100), 1);
   $percent_overall = round((($v['count'] / $total) * 100), 1);
+
+  //if $verb display is a url, grab verb of end. This is a temp hack and will be replaced before v1.0
+  if( filter_var($verb, FILTER_VALIDATE_URL) ){
+    $verb = substr(strrchr($verb, "/"), 1);
+  }
+
   $verb_url        = URL() . '/lrs/'. $lrs->_id . '/statements/verb/' . $verb; 
 ?>
   
   <tr>
-    <td class="hidden-xs">
+    <td class="hidden-xs col-sm-2">
       <span class="chart pull-left" data-percent="{{ $percent }}"></span>
     </td>
-    <td>
+    <td class="col-sm-2">
       <span class="label label-default">{{ $v['count'] }}</span>
     </td>
-    <td>
+    <td class="col-sm-2">
       <a href="{{ $verb_url }}">{{ $verb }}</a>
     </td>
-    <td>
+    <td class="col-sm-2">
       <span class="label label-default">{{ $percent }}%</span>
     </td>
-    <td>
+    <td class="col-sm-2">
       <span class="label label-default">{{ $percent_overall }}%</span>
     </td>
   </tr>
