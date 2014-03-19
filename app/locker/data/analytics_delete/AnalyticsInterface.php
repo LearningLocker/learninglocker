0de2d0798d02d0a4706671a2fc07a787df251e94
Dave Tosh <davidgtosh@gmail.com>
2014-03-19 16:13:28 -0400

    Remove old code

--- app/locker/data/analytics_delete/AnalyticsInterface.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Locker\Data\Analytics;
-
-interface AnalyticsInterface {
-
-  public function getAnalytics( $lrs );
-
-}
--- app/locker/data/old_analytics/AnalyticsInterface_old.php
+++
@@ -1,7 +0,0 @@
-<?php namespace Locker\Data\AnalyticsOld;
-
-interface AnalyticsInterface {
-
-  public function getAnalytics( $lrs );
-
-}
