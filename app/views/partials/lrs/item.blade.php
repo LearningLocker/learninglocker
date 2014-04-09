1d180b91ee4b7acc718435eecd3ffeaab9e90efb
Dave Tosh <davidgtosh@gmail.com>
2014-04-09 09:40:03 -0400

    edit and delete wired up

--- app/views/partials/lrs/item.blade.php
+++ app/views/partials/lrs/item.blade.php
@@ -13,12 +13,12 @@
   </td>
   <td>
     @if ( app\locker\helpers\Lrs::lrsEdit($lrs) )
-      <a href="{{ URL() }}/lrs/<%= _id %>/edit" class="btn btn-xs btn-success btn-space" title="{{ Lang::get('site.edit') }}"><i class="icon-pencil"></i></a>
+      <a href="{{ URL() }}/lrs/{{ $lrs->_id }}/edit" class="btn btn-xs btn-success btn-space" title="{{ Lang::get('site.edit') }}"><i class="icon-pencil"></i></a>
     @endif
   </td>
   <td>
     @if ( app\locker\helpers\Lrs::lrsEdit($lrs) )
-      <button class="btn btn-danger btn-xs delete" title="{{ Lang::get('site.delete') }}"><i class="icon-trash"></i></button>
+      @include('partials.lrs.forms.delete')
     @endif
   </td>
 </tr>
