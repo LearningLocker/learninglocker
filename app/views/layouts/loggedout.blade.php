84aac5e5ed004d9509846a733ccc23ea7e2c41f2
Dave Tosh <davidgtosh@gmail.com>
2014-04-24 10:21:49 -0400

    Move register and password buttons

--- app/views/layouts/loggedout.blade.php
+++ app/views/layouts/loggedout.blade.php
@@ -2,34 +2,37 @@
 
 @section('body')
 
-<div class="row">
+  <div class="row">
     <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-lg-offset-4 col-md-offset-3">
-        <div class="logo">
-          @if( isset($site->name) )
-            {{ $site->name }}
-          @else
-            <a href="{{ URL() }}">
-                <img src="{{ URL() }}/assets/img/logo2-grey.png" alt="Logo" />
-            </a>
-          @endif
-        </div>
-        <div class="wrapper">
+      
+      <div class="logo">
+        <img src="{{ URL() }}/assets/img/logo2-grey.png" alt="Logo" />
+      </div>
+      <div class="wrapper">
 
-          @if($errors->any())
-      <ul class="alert alert-danger">
-        {{ implode('', $errors->all('<li>:message</li>'))}}
-      </ul>
-      @endif
+        @if($errors->any())
+          <ul class="alert alert-danger">
+            {{ implode('', $errors->all('<li>:message</li>'))}}
+          </ul>
+        @endif
   
-      @yield('content')
+        @yield('content')
 
-    </div>
+      </div>
 
-    <div class="links">
-      Powered by <a href="http://learninglocker.net">Learning Locker</a>
-    </div>
-    </div>
-</div>
+      <div class="login-options">
+        @if( \Site::first()->registration === 'Open' )
+          <a href="{{ URL() }}/register" class="btn btn-sm btn-primary btn-login-options">Register</a>
+        @endif
+        <a href="{{ URL() }}/password/reset" class="btn btn-sm btn-default btn-login-options">Forgotten password</a>
+      </div>
+
+      <div class="links">
+        Powered by <a href="http://learninglocker.net">Learning Locker</a>
+      </div>
+
+    </div>  
+  </div>
 
 @show
 
