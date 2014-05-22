6f1a15891d09348a21f9144a5d4e3910c7025b50
Andy Truong <andy@toila.net>
2014-05-22 15:49:50 +0700

    Update StatementController.php
    Avoid error when old calling to StatementController::saveStatement() without attachment value.

--- app/controllers/xapi/StatementController.php
+++ app/controllers/xapi/StatementController.php
@@ -103,7 +103,7 @@ class StatementsController extends BaseController {
    * @param array $attachments
    * @return response
    */
-  public function saveStatement( $statements, $attachments ){
+  public function saveStatement( $statements, $attachments = '' ){
   
     $save = $this->statement->create( $statements, $this->lrs, $attachments );
     return $save;
