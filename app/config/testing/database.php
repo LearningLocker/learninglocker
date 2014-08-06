f2b7540b3031c5ada90ef84dd5d20cc9aed815ee
Ryan Smith <0ryansmith1994@gmail.com>
2014-08-06 10:43:34 +0100

    Changes database config.

--- app/config/testing/database.php
+++ app/config/testing/database.php
@@ -4,11 +4,11 @@ return array(
   'connections' => array(
     'mongodb' => array(
       'driver'   => 'mongodb',
-      'host'     => 'localhost',
+      'host'     => '127.0.0.1',
       'port'     => 27017,
       'username' => '',
       'password' => '',
-      'database' => 'test'
+      'database' => 'lltest'
     )
   )
 );
