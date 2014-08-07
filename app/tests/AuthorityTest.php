4151d3b3ba81101ed84595db3306e3adc83e83d6
Ryan Smith <0ryansmith1994@gmail.com>
2014-08-07 09:42:56 +0100

    Corrects indentation.

--- app/tests/AuthorityTest.php
+++ app/tests/AuthorityTest.php
@@ -8,42 +8,42 @@
 class AuthorityTest extends TestCase
 {
 
-    public $lrs;
-    protected $statement;
+  public $lrs;
+  protected $statement;
 
-    public function setUp()
-    {
-        parent::setUp();
-        // Authentication as super user.
-        $user = User::firstOrCreate(array('email' => $this->dummyEmail()));
-        Auth::login($user);
-        $this->lrs = $this->createLRS();
-        $this->statement = App::make('Locker\Repository\Statement\EloquentStatementRepository');
-    }
+  public function setUp()
+  {
+    parent::setUp();
+    // Authentication as super user.
+    $user = User::firstOrCreate(array('email' => $this->dummyEmail()));
+    Auth::login($user);
+    $this->lrs = $this->createLRS();
+    $this->statement = App::make('Locker\Repository\Statement\EloquentStatementRepository');
+  }
 
-    public function testAuthority()
-    {
-        $stmt = $this->defaultStatment();
+  public function testAuthority()
+  {
+    $stmt = $this->defaultStatment();
 
-        // Ensure if authority is empty the LRS will be create anonymous authoriry
-        $authority = $stmt['authority'];
-        unset($stmt['authority']);
-        $return = $this->createStatement($stmt, $this->lrs);
-        $this->assertEquals($return['success'], true);
+    // Ensure if authority is empty the LRS will be create anonymous authoriry
+    $authority = $stmt['authority'];
+    unset($stmt['authority']);
+    $return = $this->createStatement($stmt, $this->lrs);
+    $this->assertEquals($return['success'], true);
 
-        $stmt_id = reset($return['ids']);
-        $obj_stmt = $this->statement->find($stmt_id);
-        $stmt_authority = $obj_stmt->statement['authority'];
-        $this->assertTrue(!empty($stmt_authority));
+    $stmt_id = reset($return['ids']);
+    $obj_stmt = $this->statement->find($stmt_id);
+    $stmt_authority = $obj_stmt->statement['authority'];
+    $this->assertTrue(!empty($stmt_authority));
 
-        // Ensure authority stored in db is same value with statment send to LRS
-        $stmt['authority'] = $authority;
-        $return = $this->createStatement($stmt, $this->lrs);
-        $stmt_id = reset($return['ids']);
-        $obj_stmt = $this->statement->find($stmt_id);
+    // Ensure authority stored in db is same value with statment send to LRS
+    $stmt['authority'] = $authority;
+    $return = $this->createStatement($stmt, $this->lrs);
+    $stmt_id = reset($return['ids']);
+    $obj_stmt = $this->statement->find($stmt_id);
 
-        $stmt_authority = $obj_stmt->statement['authority'];
-        $this->assertEquals($authority, $stmt_authority);
-    }
+    $stmt_authority = $obj_stmt->statement['authority'];
+    $this->assertEquals($authority, $stmt_authority);
+  }
 
 }
