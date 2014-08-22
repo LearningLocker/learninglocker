12f2c5ba2f9db3b072fd79e06ac96a35cca9dfd2
Ryan Smith <0ryansmith1994@gmail.com>
2014-08-22 17:25:07 +0100

    Adds events to field views.

--- public/assets/js/exports/exports/fields/modelView.js
+++ public/assets/js/exports/exports/fields/modelView.js
@@ -2,6 +2,13 @@ define([
   'marionette'
 ], function (Marionette) {
   return Marionette.ItemView.extend({
-    template: '#'
+    template: '#',
+    events: {
+      'click #remove': 'remove'
+    },
+
+    remove: function () {
+      this.model.destroy();
+    }
   });
 });
