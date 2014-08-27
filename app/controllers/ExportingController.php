f391fd1abc30718ff4f8d2132d5d9235e8520c1c
Ryan Smith <0ryansmith1994@gmail.com>
2014-08-27 15:59:18 +0100

    Delivers exports view via ExportingController.

--- app/controllers/ExportingController.php
+++ app/controllers/ExportingController.php
@@ -13,11 +13,10 @@ class ExportingController extends \BaseController {
   protected $export;
   protected $params;
 
-  public function __construct(Analytics $analytics, Lrs $lrs, Query $query, Report $report){
+  public function __construct(Analytics $analytics, Lrs $lrs, Query $query){
     $this->analytics = $analytics;
     $this->lrs       = $lrs;
     $this->query     = $query;
-    $this->export    = $report;
     
     $this->beforeFilter('auth');
     $this->beforeFilter('auth.lrs'); //check user can access LRS.
@@ -33,130 +32,11 @@ class ExportingController extends \BaseController {
   public function index($id){
     $lrs      = $this->lrs->find( $id );
     $lrs_list = $this->lrs->all();
-    $exports  = $this->export->all($id);
-    return View::make('partials.exporting.index', [
+    return View::make('partials.exports.index', [
       'lrs' => $lrs, 
       'list' => $lrs_list,
-      'exporting_nav' => true,
-      'exports' => $exports
-    ]);
-  }
-
-  public function getData($id) {
-    $fields = json_decode($this->params['fields'], true);
-    $result = $this->query->selectFields($id, array_keys($fields));
-    return Response::json($result);
-  }
-
-  /**
-   * Return json feed of reports
-   *
-   * @return response
-   **/
-  public function getReports($id, $limit=5){
-    $exports = $this->export->all($id);
-    return Response::json( $exports );
-  }
-
-  /**
-   * Show the form for creating a new report.
-   *
-   * @param string $id Lrs ID
-   * @return Response
-   */
-  public function create($id){
-    $lrs      = $this->lrs->find( $id );
-    $lrs_list = $this->lrs->all();
-
-    return View::make('partials.exporting.create', [
-      'lrs' => $lrs,
-      'list' => $lrs_list,
       'exporting_nav' => true
     ]);
-  }
-
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @return Response
-   */
-  public function store(){
-    $request  = \Request::instance();
-    $incoming = $request->getContent();
-    $data     = json_decode($incoming, true);
-
-    //lrs input validation
-    $rules['name']         = 'required';
-    $validator = Validator::make($data, $rules);
-    if ($validator->fails())  return \Response::json( $validator );
-
-    $save = $this->export->create( $data );
-
-    if( $save ){
-      return \Response::json( 'success' );
-    }
-
-    return \Response::json( 'error' );
-  }
-
-  /**
-   * Display the specified resource.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function show($id, $report){
-    $lrs      = $this->lrs->find($id);
-    $lrs_list = $this->lrs->all();
-    $export   = $this->export->find($export);
-    return View::make('partials.exporting.view', [
-      'lrs'        => $lrs,
-      'list'       => $lrs_list,
-      'export'     => $export, 
-      'exporting_nav' => true
-    ]);
-  }
-
-  /**
-   * Loop through saved report query and decode any url's
-   *
-   **/
-  public function decodeURL($array){
-    $output = '';
-
-    if( !empty($array) ){
-      foreach($array as $key => $value){
-        if(is_array($value)){
-          $output[$key] = $this->decodeURL( $value );
-        }else{
-          $output[$key] = urldecode($value);
-        }
-      }
-    }
-
-    return $output;
-  }
-
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function destroy($id, $export){
-    $export = $this->export->delete($export);
-    return Redirect::back()->with('success', Lang::get('exporting.deleted')); 
-  }
-
-  /**
-   * Return available objects for typeahead. Used to search activities 
-   * as well as parent and groups id in context.
-   * 
-   * @return results or empty string
-   *
-   **/
-  public function getTypeahead() {
-    return Response::json(['actor']);
   }
 
   /**
