1f145ea5dade21732e63cf5a9328c071fb257bdc
Ryan Smith <0ryansmith1994@gmail.com>
2014-09-03 17:06:57 +0100

    Quotes fields if necessary.

--- app/locker/data/exporter/Exporter.php
+++ app/locker/data/exporter/Exporter.php
@@ -11,6 +11,43 @@ class Exporter {
     # code...
   }
 
+  /**
+   * Adds quotes to a field that already contains quotes.
+   * @param  String $field The field to be quoted.
+   * @return String        The correctly quoted field.
+   */
+  private function quoteQuotedField ( $field ) {
+    $splits = explode('"', $field);
+    $quotedField = array_shift($splits);
+
+    foreach ($splits as $split) {
+      $quotedField .= '"' . $split . '"';
+    }
+
+    return $quotedField;
+  }
+
+  /**
+   * Adds quotes to a field if necessary.
+   * @param  String $field The field to be quoted.
+   * @return String        The correctly quoted field.
+   */
+  private function quoteField ( $field ) {
+    if (strpos($field, '"') !== false) {
+      return quoteQuotedField($field);
+    } else if (strpos($field, ',') !== false) {
+      return '"' . $field . '"';
+    } else {
+      return $field;
+    }
+  }
+
+  /**
+   * Maps values from old keys to new keys.
+   * @param  Array $statements The statements to be mapped.
+   * @param  AssocArray $fields     The fields to be mapped.
+   * @return Array             The mapped statements.
+   */
   public function mapFields ( $statements, $fields ) {
     $mappedStatements = [];
 
@@ -20,7 +57,7 @@ class Exporter {
         if (!is_null($field['to'])) {
           $mappedStatement[$field['to']] = 
             !is_null($field['from']) ?
-            $statement[$field['from']] :
+            $this->quoteField($statement[$field['from']]) :
             null;
         }
       }
@@ -31,6 +68,12 @@ class Exporter {
     return $mappedStatements;
   }
 
+  /**
+   * Filters the statements.
+   * @param  Statement $statements The statement query to be used.
+   * @param  AssocArray $fields     The fields to be mapped.
+   * @return Array             The array of filtered statements.
+   */
   public function filter ( $statements, $fields ) {
     $statementFields = [];
 
