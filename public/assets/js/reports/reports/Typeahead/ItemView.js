5003acb4785a94340c3ef2a3f293fdba849e8cdd
Ryan Smith <0ryansmith1994@gmail.com>
2014-10-13 17:17:45 +0100

    Creates typeahead item view.

---
+++ public/assets/js/reports/reports/Typeahead/ItemView.js
@@ -0,0 +1,58 @@
+define([
+  'underscore',
+  'locker',
+  'jQuery'
+  'typeahead'
+], function (_, locker, jQuery) {
+  return locker.ItemView.extend({
+    typeaheadUrl: '',
+    _matchTypeahead: function (query, cb) {
+      var matches = [];
+      var substrRegex = new RegExp(query, 'i');
+   
+      $.each(this._typeaheadKeys, function (i, str) {
+        if (substrRegex.test(str)) {
+          matches.push({value: str});
+        }
+      });
+
+      cb(matches);
+    },
+    _typeaheadKeys: [],
+    _initialiseTypeahead: function () {
+      var trigger = (function () {
+        this.ui.typeahead.trigger('change');
+      }).bind(this);
+
+      this.ui.typeahead.typeahead({
+        minlength: 1
+      }, {
+        name: 'keys',
+        displayKey: 'value',
+        source: this._matchTypeahead
+      }).on(
+        'typeahead:selected',
+        trigger
+      ).on(
+        'typeahead:autocompleted',
+        trigger
+      );
+    }
+    _requestKeys: function () {
+      jQuery.ajax(this.typeaheadUrl, {
+        beforeSend: function (xhr) {
+          var auth = btoa(window.lrs.key + ':' + window.lrs.secret);
+          xhr.setRequestHeader ('Authorization', 'Basic ' + auth);
+        },
+      }).done(function (data, status, xhr) {
+        this._typeaheadKeys = data;
+        this._initialiseTypeahead();
+      }).fail(function (xhr, status, error) {
+
+      });
+    },
+    onRender: function () {
+      this._requestKeys();
+    }
+  });
+});
