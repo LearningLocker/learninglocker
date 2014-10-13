8d9f712754b32b84e852e5cdeb457e5135af097f
Ryan Smith <0ryansmith1994@gmail.com>
2014-10-13 13:08:55 +0100

    Adds defaults to report model.

--- public/assets/js/reports/reports/model.js
+++ public/assets/js/reports/reports/model.js
@@ -4,6 +4,12 @@ define([
 ], function(locker) {
   return locker.Model.extend({
     idAttribute: '_id',
+    defaults: {
+      _id: null,
+      name: null,
+      description: null,
+      lrs: window.lrsId
+    },
     credentials: {
       username: window.lrs.key,
       password: window.lrs.secret
