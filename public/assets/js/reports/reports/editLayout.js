a6b80ce8ea95807fa173dea75c8882effa4ab661
Ryan Smith <0ryansmith1994@gmail.com>
2014-10-13 17:18:05 +0100

    Starts to add relations.

--- public/assets/js/reports/reports/editLayout.js
+++ public/assets/js/reports/reports/editLayout.js
@@ -1,8 +1,56 @@
 define([
   'locker',
+  './TypeAhead/ItemView'
   'text!./editLayout.html'
-], function(Locker, template) {
-  return Locker.LayoutView.extend({
-    template: template
+], function(locker, TypeaheadView, template) {
+  return locker.LayoutView.extend({
+    template: template,
+    relations: {
+      actors: locker.CollectionView.extend({
+        itemView: TypeaheadView.extend({
+          typeaheadUrl: '../../api/v1/bla'
+        })
+      }),
+      verbs: locker.CollectionView.extend({
+        itemView: TypeaheadView.extend({
+          typeaheadUrl: '../../api/v1/bla'
+        })
+      }),
+      activities: locker.CollectionView.extend({
+        itemView: TypeaheadView.extend({
+          typeaheadUrl: '../../api/v1/bla'
+        })
+      }),
+      activityTypes: locker.CollectionView.extend({
+        itemView: TypeaheadView.extend({
+          typeaheadUrl: '../../api/v1/bla'
+        })
+      }),
+      parents: locker.CollectionView.extend({
+        itemView: TypeaheadView.extend({
+          typeaheadUrl: '../../api/v1/bla'
+        })
+      }),
+      groups: locker.CollectionView.extend({
+        itemView: TypeaheadView.extend({
+          typeaheadUrl: '../../api/v1/bla'
+        })
+      }),
+      platforms: locker.CollectionView.extend({
+        itemView: TypeaheadView.extend({
+          typeaheadUrl: '../../api/v1/bla'
+        })
+      }),
+      instructors: locker.CollectionView.extend({
+        itemView: TypeaheadView.extend({
+          typeaheadUrl: '../../api/v1/bla'
+        })
+      }),
+      languages: locker.CollectionView.extend({
+        itemView: TypeaheadView.extend({
+          typeaheadUrl: '../../api/v1/bla'
+        })
+      })
+    }
   });
 });
--- public/assets/js/reports/reports/model.js
+++ public/assets/js/reports/reports/model.js
@@ -1,7 +1,8 @@
 define([
   'locker',
+  'backbone',
   'basicauth'
-], function(locker) {
+], function(locker, backbone) {
   return locker.Model.extend({
     idAttribute: '_id',
     defaults: {
@@ -10,9 +11,24 @@ define([
       description: null,
       lrs: window.lrsId
     },
+    relations: {
+      actors: backbone.Collection,
+      verbs: backbone.Collection,
+      activities: backbone.Collection,
+      activityTypes: backbone.Collection,
+      parents: backbone.Collection,
+      groups: backbone.Collection,
+      platforms: backbone.Collection,
+      instructors: backbone.Collection,
+      languages: backbone.Collection
+    },
     credentials: {
       username: window.lrs.key,
       password: window.lrs.secret
+    },
+    _initializeRelations: function (response, empty) {
+      // @todo: Map `response.query` to expected relations.
+      return locker.Model.prototype._initializeRelations(response, empty);
     }
   });
 });
