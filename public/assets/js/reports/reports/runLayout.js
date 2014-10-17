da985bb1cbd1cd5d58d9552855817daa783d7d5e
Ryan Smith <0ryansmith1994@gmail.com>
2014-10-17 17:12:32 +0100

    Adds run layout.

--- public/assets/js/reports/reports/runLayout.js
+++ public/assets/js/reports/reports/runLayout.js
@@ -1,8 +1,26 @@
 define([
   'locker',
-  'text!./runLayout.html'
-], function(locker, template) {
+  'text!./runLayout.html',
+  './statements/collection',
+  './statements/composite'
+], function(locker, template, StatementsCollection, StatementsComposite) {
   return locker.LayoutView.extend({
-    template: template
+    template: template,
+    regions: {
+      graph: '#graph',
+      list: '#list'
+    },
+    onRender: function () {
+      // Gets the statements returned from running the report.
+      var statements = new StatementsCollection();
+      statements.url = this.model.url() + '/' + statements.nestedUrl;
+
+      this.list.show(new StatementsComposite({
+        collection: statements
+      }));
+      statements.fetch().success(function () {
+        
+      });
+    }
   });
 });
