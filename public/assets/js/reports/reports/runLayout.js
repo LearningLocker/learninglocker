14833b5f9de891f910cd494dbd031ac1e5be45c5
Ryan Smith <0ryansmith1994@gmail.com>
2014-10-20 16:08:26 +0100

    Adds graph, removes statements.

--- public/assets/js/reports/reports/runLayout.js
+++ public/assets/js/reports/reports/runLayout.js
@@ -1,26 +1,39 @@
 define([
+  'jquery',
   'locker',
   'text!./runLayout.html',
-  './statements/collection',
-  './statements/composite'
-], function(locker, template, StatementsCollection, StatementsComposite) {
+  'morris'
+], function(jquery, locker, template, morris) {
   return locker.LayoutView.extend({
     template: template,
-    regions: {
-      graph: '#graph',
-      list: '#list'
-    },
-    onRender: function () {
-      // Gets the statements returned from running the report.
-      var statements = new StatementsCollection();
-      statements.url = this.model.url() + '/' + statements.nestedUrl;
+    _renderGraph: function () {
+      jquery.ajax(this.model.url() + '/graph', {
+        beforeSend: function (xhr) {
+          var auth = btoa(window.lrs.key + ':' + window.lrs.secret);
+          xhr.setRequestHeader('Authorization', 'Basic ' + auth);
+        }
+      }).done(function (data) {
+        var morrisData = [];
 
-      this.list.show(new StatementsComposite({
-        collection: statements
-      }));
-      statements.fetch().success(function () {
-        
+        $.each(data, function() {
+          var setDate = this.date[0].substring(0,10);
+          var setData = { y: setDate, a: this.count, b: 2 };
+          morrisData.push(setData);
+        });
+
+        Morris.Bar({
+          element: 'graph',
+          data: morrisData,
+          xkey: 'y',
+          ykeys: ['a'],
+          labels: ['Number of statements']
+        });
       });
+    },
+    onShow: function () {
+      this._modelSuccess(function () {
+        this._renderGraph();
+      }.bind(this));
     }
   });
 });
