8986942559f35a7cc9a9c8b9ebe84b84f3b7e71b
Ryan Smith <0ryansmith1994@gmail.com>
2014-10-21 12:09:30 +0100

    Removes old views.

--- app/views/partials/reporting/forms/delete.blade.php
+++
@@ -1,4 +0,0 @@
- {{ Form::open(array('url' => 'lrs/'.$lrs.'/reporting/delete/'.$report, 
-  'method' => 'DELETE', 'class' => 'pull-right', 'onsubmit' => 'return confirm("Are you sure?")')) }}
-  <button type="submit" class="btn btn-default btn-danger btn-sm">{{ Lang::get('site.delete') }}</button>
-{{ Form::close() }}
