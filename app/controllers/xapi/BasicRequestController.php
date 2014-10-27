31ca98d6d6bde1ba6277881e8b556b0fc264f57f
Ryan Smith <0ryansmith1994@gmail.com>
2014-10-27 16:13:22 +0000

    Cleans up BasicRequestController.

--- app/controllers/xapi/BasicRequestController.php
+++ app/controllers/xapi/BasicRequestController.php
@@ -4,20 +4,11 @@ use Locker\Repository\Client\ClientRepository as Client;
 
 class BasicRequestController extends BaseController {
 
-  /**
-   * Lrs
-   **/
-  protected $lrs;
-  
-  /**
-   * Client
-   **/
+  // Defines properties to be set to constructor parameters.
   protected $client;
 
-  /**
-   * Filter parameters
-   **/
-  protected $params;
+  // Defines properties to be set by filters.
+  protected $params, $lrs;
 
 
   /**
@@ -27,49 +18,30 @@ class BasicRequestController extends BaseController {
    *  @param Client $client
    */
   public function __construct(Client $client){
-  	
-	$this->client  = $client;
-
+  	$this->client  = $client;
     $this->beforeFilter('@getLrs');
- 	$this->beforeFilter('@setParameters', array('except' => 'store', 'put'));
-
-
+    $this->beforeFilter('@setParameters', ['except' => 'store', 'put']);
   }
 
   /**
-   * Create a client and return the credentials. Handles POST
-   *
-   *
+   * Create a client and return the credentials.
    * @return Response
    */
   public function store(){
-
-    //grab incoming statement
-    $request = \Request::instance();
-    $content = $request->getContent();
+    $content = \LockerRequest::getContent();
 	
-	$data = array('lrs_id' => $this->lrs->_id);
-	
-	$client = $this->client->create( $data );
+  	$data = ['lrs_id' => $this->lrs->_id];
+  	$client = $this->client->create( $data );
 
     if($client){
     	$returnCredentials = array(
     		'key' => $client->api['basic_key'],
     		'secret' => $client->api['basic_secret']
-		);
+		  );
 		
       return \Response::json($returnCredentials, 200 );
-    }else{
-      return \Response::json( "I'm a teapot", 418 ); //TODO: think through proper error handling and validation
-    }     
-      
-	  
-    //$save = $this->saveStatement( $statements, $attachments );
-    //return $this->sendResponse( $save );
-
+    } else {
+      return \Response::json('I\'m a teapot', 418 );
+    }
   }
-  
-
-
-
 }
