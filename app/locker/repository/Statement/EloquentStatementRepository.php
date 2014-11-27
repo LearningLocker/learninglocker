97e1cfd68685eaadfbc64d734b740eaa715af3ad
Ryan Smith <0ryansmith1994@gmail.com>
2014-11-27 10:59:44 +0000

    Fixes statement refs.

--- app/locker/repository/Statement/EloquentStatementRepository.php
+++ app/locker/repository/Statement/EloquentStatementRepository.php
@@ -30,7 +30,7 @@ class EloquentStatementRepository implements StatementRepository {
   /**
    * Gets the statement with the given $id from the lrs (with the $lrsId).
    * @param UUID $lrsId
-   * @param UUID $id 
+   * @param UUID $id
    * @param boolean $voided determines if the statement is voided.
    * @param boolean $active determines if the statement is active.
    * @return Builder
@@ -218,7 +218,7 @@ class EloquentStatementRepository implements StatementRepository {
 
   /**
    * Attempts to convert a $langMap to a single string using a relevant language from $langs.
-   * @param [LanguageMap] $langMap 
+   * @param [LanguageMap] $langMap
    * @param [Language] $langs
    * @return String/[LanguageMap]
    */
@@ -275,7 +275,7 @@ class EloquentStatementRepository implements StatementRepository {
    */
   private function getStatementIds(array $statement) {
     $actor = $statement['actor'];
-    
+
     // Processes an anonymous group or actor.
     if (isset($actor['objectType']) && $actor['objectType'] === 'Group' && $this->getAgentIdentifier($actor) === null) {
       $members = [];
@@ -410,13 +410,13 @@ class EloquentStatementRepository implements StatementRepository {
 
       // Finds the statement that it references.
       $reference = $this->query->where($lrs->_id, [
-        ['statement.id', '=', $statement->statement['object']['id']],
-        ['refs.id', '!=', $statement->statement['id']]
-      ])->whereNotIn('statement.id', $statement->refs)->first();
+        ['statement.id', '=', $statement->statement['object']['id']]
+      ])->first();
 
       // Updates the refs.
-      $statement->refs[] = $reference->statement;
-      array_merge($statement->refs, $reference->refs);
+      if ($reference) {
+        $statement->refs = array_push($reference->refs ?: [], $reference->statement);
+      }
       if (!$statement->save()) throw new \Exception('Failed to save statement reference.');
 
       return $statement;
@@ -469,7 +469,7 @@ class EloquentStatementRepository implements StatementRepository {
       return $statement;
     }, $statements);
   }
-  
+
   /**
    * Creates $statements in the $lrs with $attachments.
    * @param [Statement] $statements
@@ -520,7 +520,7 @@ class EloquentStatementRepository implements StatementRepository {
       'stored' => ($currentDate = $this->getCurrentDate()),
       'timestamp' => $currentDate
     ], $statement);
-     
+
     // For now we store the latest submitted definition.
     // @todo this will change when we have a way to determine authority to edit.
     if( isset($statement['object']['definition'])){
@@ -588,7 +588,7 @@ class EloquentStatementRepository implements StatementRepository {
       ->where('lrs._id', $lrsId)
       ->where('statement.id', $statement['id'])
       ->first();
-    
+
     if ($existingModel) {
         $existingStatement = (array) $existingModel['statement'];
         unset($existingStatement['stored']);
@@ -597,7 +597,7 @@ class EloquentStatementRepository implements StatementRepository {
         array_multisort($statement);
         ksort($existingStatement);
         ksort($statement);
-    
+
         if ($existingStatement == $statement) {
           return $existingModel;
         } else {
@@ -625,7 +625,7 @@ class EloquentStatementRepository implements StatementRepository {
       $headers     = array();
       foreach ($raw_headers as $header) {
         list($name, $value) = explode(':', $header);
-        $headers[strtolower($name)] = ltrim($value, ' '); 
+        $headers[strtolower($name)] = ltrim($value, ' ');
       }
 
       //get the correct ext if valid
@@ -635,7 +635,7 @@ class EloquentStatementRepository implements StatementRepository {
       }
 
       $filename = str_random(12) . "." . $ext;
-      
+
       //create directory if it doesn't exist
       if (!\File::exists(base_path().'/uploads/'.$lrs.'/attachments/' . $headers['x-experience-api-hash'] . '/')) {
         \File::makeDirectory(base_path().'/uploads/'.$lrs.'/attachments/' . $headers['x-experience-api-hash'] . '/', 0775, true);
@@ -643,7 +643,7 @@ class EloquentStatementRepository implements StatementRepository {
 
       $destinationPath = base_path().'/uploads/'.$lrs.'/attachments/' . $headers['x-experience-api-hash'] . '/';
 
-      $filename = $destinationPath.$filename; 
+      $filename = $destinationPath.$filename;
       $file = fopen( $filename, 'wb'); //opens the file for writing with a BINARY (b) fla
       $size = fwrite( $file, $body ); //write the data to the file
       fclose( $file );
@@ -673,7 +673,7 @@ class EloquentStatementRepository implements StatementRepository {
 
   public function grouped($id, $parameters){
     $type = isset($parameters['grouping']) ? strtolower($parameters['grouping']) : '';
-    
+
     switch ($type) {
       case "time":
         $interval = isset($parameters['interval']) ? $parameters['interval'] : "day";
