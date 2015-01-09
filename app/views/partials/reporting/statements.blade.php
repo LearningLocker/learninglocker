e34dc28a139cfa0a71840765f2bbcac8cbd9d11c
Ryan Smith <0ryansmith1994@gmail.com>
2015-01-09 12:25:29 +0000

    Fixes heading for report statements.

--- app/views/partials/reporting/statements.blade.php
+++ app/views/partials/reporting/statements.blade.php
@@ -1,7 +1,27 @@
 @extends('partials.statements.list')
 
 @section('title')
-  @include('partials.site.elements.page_title', array('page' => Lang::get('statements.reporting') ))
+  <div class="page-header">
+    <h1>{{ Lang::get('statements.reporting') }}<span style="margin-left: 10px; font-size:15px; vertical-align:middle;"><i class="icon icon-question-sign" data-toggle="modal" data-target="#myModal"></i></span></h1>
+
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel">What is a report?</h4>
+          </div>
+          <div class="modal-body">
+            <p>Reports allow you to filter statements that were sent to the LRS by a range of criteria; this is useful if you just want to see all the statements for a particular actor, activity, etc. or a combination thereof.</p><p>For example, a report would allow you to retrieve all of the statements made by Joe Bloggs in Decemeber on the "Maths 101" course.</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <ol class="breadcrumb">
     <li><a href="{{ route('reporting.index', [$report->lrs]) }}" data-toggle="tooltip" data-placement="bottom" title="View all of the reports for this LRS">{{ Lang::get('statements.reporting') }}</a></li>
     <li class="active">{{$report->name}}</li>
