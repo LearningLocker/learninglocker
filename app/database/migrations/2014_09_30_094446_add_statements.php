559a96d3d2ddbd8cb42e635912616417d2402ab1
Ryan Smith <0ryansmith1994@gmail.com>
2015-01-15 16:49:35 +0000

    Adds some indexes and fixes `down`.

--- app/database/migrations/2014_09_30_094446_add_statements.php
+++ app/database/migrations/2014_09_30_094446_add_statements.php
@@ -12,10 +12,27 @@ class AddStatements extends Migration {
 	 */
 	public function up() {
 		Schema::table('statements', function (Blueprint $table) {
-      $table->index('lrs._id');
-      $table->index(array('lrs._id', 'statement.object.id'));
-      $table->index(array('lrs._id', 'statement.verb.id'));
-      $table->index(array('lrs._id', 'statement.actor.mbox'));
+      $table->index('lrs._id', [
+        'name' => 'lrs._id_index'
+      ]);
+      $table->index(['lrs._id', 'statement.object.id'], [
+        'name' => 'lrs._id_statement.object.id_index'
+      ]);
+      $table->index(['lrs._id', 'statement.verb.id'], [
+        'name' => 'lrs._id_statement.verb.id_index'
+      ]);
+      $table->index(['lrs._id', 'statement.actor.mbox'], [
+        'name' => 'lrs._id_statement.actor.mbox_index'
+      ]);
+      $table->index(['lrs._id', 'timestamp'], [
+        'name' => 'lrs._id_timestamp_index'
+      ]);
+      $table->index(['statement.stored'], [
+        'name' => 'statement.stored_index'
+      ]);
+      $table->index(['statement.stored', 'lrs._id'], [
+        'name' => 'statement.stored_lrs._id_index'
+      ]);
 		});
 	}
 
@@ -27,7 +44,13 @@ class AddStatements extends Migration {
 	 */
 	public function down() {
 		Schema::table('statements', function (Blueprint $table) {
-			
+			$table->dropIndex('lrs._id_index');
+      $table->dropIndex('lrs._id_statement.object.id_index');
+      $table->dropIndex('lrs._id_statement.verb.id_index');
+      $table->dropIndex('lrs._id_statement.actor.mbox_index');
+      $table->dropIndex('lrs._id_timestamp_index');
+      $table->dropIndex('statement.stored_index');
+      $table->dropIndex('statement.stored_lrs._id_index');
 		});
 	}
 }
