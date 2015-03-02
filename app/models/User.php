5f76e0765941f33ab33c6e559b684715af74e15e
Ryan Smith <0ryansmith1994@gmail.com>
2015-03-02 14:30:10 +0000

    Tidies up User model and fillable.

--- app/models/User.php
+++ app/models/User.php
@@ -6,75 +6,24 @@ use Jenssegers\Mongodb\Model as Eloquent;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
-  protected $guarded = array('_id');
-
-  /**
-   * Our MongoDB collection used by the model.
-   *
-   * @var string
-   */
+  protected $guarded = ['_id'];
   protected $collection = 'users';
-
-  /**
-   * The attributes excluded from the model's JSON form.
-   *
-   * @var array
-   */
-  protected $hidden = array('password');
-
-  protected $rules = array(
-      'email'    => 'required|email|unique:users',
-      'name'     => 'required',
-      'password' => 'required|confirmed'
-    );
+  protected $hidden = ['password'];
+  protected $fillable = ['email'];
+  protected $rules = [
+    'email'    => 'required|email|unique:users',
+    'name'     => 'required',
+    'password' => 'required|confirmed'
+  ];
 
   public function validate( $data ) {
     return Validator::make($data, $this->rules);
   }
 
-  /**
-   * Get the unique identifier for the user.
-   *
-   * @return mixed
-   */
-  public function getAuthIdentifier()
-  {
-    return $this->getKey();
-  }
-
-  /**
-   * Get the password for the user.
-   *
-   * @return string
-   */
-  public function getAuthPassword()
-  {
-    return $this->password;
-  }
-
-  /**
-   * Get the e-mail address where password reminders are sent.
-   *
-   * @return string
-   */
-  public function getReminderEmail()
-  {
-    return $this->email;
-  }
-
-  public function getRememberToken()
-  {
-      return $this->remember_token;
-  }
-
-  public function setRememberToken($value)
-  {
-      $this->remember_token = $value;
-  }
-
-  public function getRememberTokenName()
-  {
-      return 'remember_token';
-  }
-
+  public function getAuthIdentifier() { return $this->getKey(); }
+  public function getAuthPassword() { return $this->password; }
+  public function getReminderEmail() { return $this->email; }
+  public function getRememberToken() { return $this->remember_token; }
+  public function setRememberToken($value) { $this->remember_token = $value; }
+  public function getRememberTokenName() { return 'remember_token'; }
 }
