40ceaff40cca3ce1850c6c6b13e3cd5a2d429f05
Ryan Smith <0ryansmith1994@gmail.com>
2015-03-19 12:51:59 +0000

    Improves version displayed.

--- app/views/layouts/sidebars/sidebar_footer.blade.php
+++ app/views/layouts/sidebars/sidebar_footer.blade.php
@@ -3,6 +3,12 @@
   <div id="footer">
     Powered by
     <a href="http://learninglocker.net" target='_blank'>Learning Locker</a>
-    <a href="https://github.com/LearningLocker/learninglocker/releases/tag/v{{ file_get_contents(base_path().'/VERSION') }}">Version {{ file_get_contents(base_path().'/VERSION') }}</a>
-    <a href="https://github.com/LearningLocker/learninglocker/releases/latest">Get the latest version</a>
+    <?php $installed_version = file_get_contents(base_path().'/VERSION'); ?>
+    <?php $latest_version = file_get_contents('https://raw.githubusercontent.com/LearningLocker/learninglocker/master/VERSION'); ?>
+    <a href="https://github.com/LearningLocker/learninglocker/releases/tag/v{{ $installed_version }}">Version {{ $installed_version }}</a>
+    @if ($latest_version !== $installed_version)
+      <a href="https://github.com/LearningLocker/learninglocker/releases/latest">You can upgrade to {{ $latest_version }}</a>
+    @else
+      <a href="https://github.com/LearningLocker/learninglocker/releases/latest">You have the latest version</a>
+    @endif
   </div>
