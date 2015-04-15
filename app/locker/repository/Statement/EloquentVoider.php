e4f39e4d1f8ecb345e7d84677bbaec7f0c999137
Ryan Smith <0ryansmith1994@gmail.com>
2015-04-15 14:59:33 +0100

    Creates statement voider.

---
+++ app/locker/repository/Statement/EloquentVoider.php
@@ -0,0 +1,60 @@
+<?php namespace Locker\Repository\Statement;
+
+use \Illuminate\Database\Eloquent\Model as Model;
+
+interface VoiderInterface {
+  public function voidStatements(array $statements, array $opts);
+}
+
+class EloquentVoider extends EloquentLinker implements VoiderInterface {
+
+  /**
+   * Voids statements that need to be voided.
+   * @param [\stdClass] $statements
+   * @param [String => Mixed] $opts
+   */
+  public function voidStatements(array $statements, array $opts) {
+    return array_map(function (\stdClass $voider) use ($opts) {
+      return $this->voidStatement($voider, $opts);
+    }, $statements);
+  }
+
+  /**
+   * Voids a statement if it needs to be voided.
+   * @param \stdClass $voider
+   * @param [String => Mixed] $opts
+   */
+  private function voidStatement(\stdClass $voider, array $opts) {
+    if (!$this->isVoiding($voider)) return;
+
+    $voided = $this->where($opts)
+      ->where('statement.id', $voider->object->id)
+      ->first();
+
+    if ($voided !== null) {
+      if ($this->isVoidinging($voided->statement)) throw new \Exception(trans(
+        'xapi.errors.void_voider'
+      ));
+
+      $voided->voided = true;
+      $voided->save();
+    } else {
+      throw new \Exception(trans(
+        'xapi.errors.void_null'
+      ));
+    }
+  }
+
+  /**
+   * Determines if a statement is a voiding statement.
+   * @param \stdClass $voider
+   * @return Boolean
+   */
+  private function isVoiding(\stdClass $voider) {
+    return (
+      isset($voider->object->id) &&
+      $voider->object->id === 'http://adlnet.gov/expapi/verbs/voided' &&
+      $this->isReferencing($statement)
+    );
+  }
+}
