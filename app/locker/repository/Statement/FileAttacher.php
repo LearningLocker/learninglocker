fa90459f4dbbbc4eb8d61b26eac8dea390bf632b
Ryan Smith <0ryansmith1994@gmail.com>
2015-04-29 12:56:50 +0100

    Update FileAttacher.php

--- app/locker/repository/Statement/FileAttacher.php
+++ app/locker/repository/Statement/FileAttacher.php
@@ -15,8 +15,7 @@ class FileAttacher {
   public function store(array $attachments, array $hashes, StoreOptions $opts) {
     $dir = $this->getDir($opts);
     if (!is_dir($dir) && count($attachments > 0) && !empty($attachments)) {
-      echo $dir;
-      mkdir($dir, null, true);
+      mkdir($dir, 0775, true);
     }
 
     foreach ($attachments as $attachment) {
