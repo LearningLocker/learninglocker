2c536cfc28a5b5017e9991797eb4c2160e809993
Ryan Smith <0ryansmith1994@gmail.com>
2015-05-07 12:08:08 +0100

    Improves OAuth tests.

--- app/tests/routes/OAuthTest.php
+++ app/tests/routes/OAuthTest.php
@@ -14,7 +14,7 @@ class OAuthTest extends \Tests\LrsTestCase {
 
   protected function requestToken($client_id, $client_secret, $grant_type = 'client_credentials') {
     $method = 'POST';
-    $uri = 'oauth/access_token';
+    $uri = '/oauth/access_token';
     $params = [
       'client_id' => $client_id,
       'client_secret' => $client_secret,
@@ -25,10 +25,47 @@ class OAuthTest extends \Tests\LrsTestCase {
     return $this->request($method, $uri, $params, $server, $content);
   }
 
+  protected function requestApi($token) {
+    $method = 'GET';
+    $uri = '/data/xAPI/statements';
+    $params = [];
+    $server = [
+      'HTTP_Authorization' => 'Bearer '.$token,
+      'HTTP_X-Experience-API-Version' => '1.0.1'
+    ];
+    $content = null;
+    return $this->request($method, $uri, $params, $server, $content);
+  }
+
   public function testAccessTokenViaClient() {
     $response = $this->requestToken($this->lrs->api['basic_key'], $this->lrs->api['basic_secret']);
     $data = json_decode($response->getContent());
-    dd($data);
+    
+    $this->assertEquals(true, is_object($data));
+
+    // Checks properties of result object.
+    $this->assertEquals(true, isset($data->access_token));
+    $this->assertEquals(true, isset($data->expires_in));
+    $this->assertEquals(true, isset($data->token_type));
+
+    // Checks property types of result object.
+    $this->assertEquals(true, is_string($data->access_token));
+    $this->assertEquals(true, is_int($data->expires_in));
+    $this->assertEquals(true, is_string($data->token_type));
+
+    // Checks property values of result object.
+    $this->assertEquals(3600, $data->expires_in);
+    $this->assertEquals('Bearer', $data->token_type);
+  }
+
+  public function testAccessViaToken() {
+    $token_response = $this->requestToken($this->lrs->api['basic_key'], $this->lrs->api['basic_secret']);
+    $token = json_decode($token_response->getContent())->access_token;
+    $response = $this->requestApi($token);
+    $data = $response->getContent();
+
+    // Checks result object.
+    $this->assertEquals('{"more":"","statements":[]}', $data);
   }
 
   public function tearDown() {
