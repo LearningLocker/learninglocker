1aff7e67bb59f58ee6632eeceec6b690233b094d
Ryan Smith <0ryansmith1994@gmail.com>
2015-07-03 12:04:14 +0100

    Removes output to reduce Travis log.

--- test.sh
+++ test.sh
@@ -14,13 +14,13 @@ if [ "${DEVELOP}" = "${TRAVIS_BRANCH}" ]; then
   mongo lltest --eval 'db.client.insert({"api" : { "basic_key" : "1484c2ac05269b8c5479a1dd6a0d6370991fd6a1", "basic_secret" : "f0ef3d8062805c0fc1675beb8ac0715c75df13cb" }, "lrs_id" : db.lrs.find()[0]._id, "authority" : { "name" : "New Client", "mbox" : "mailto:hello@learninglocker.net" }, "scopes" : [ "all" ]})'
 
   # Runs the test suite.
-  git clone https://github.com/ryansmith94/xAPI_LRS_Test.git conformance
+  git clone https://github.com/ryansmith94/xAPI_LRS_Test.git conformance > /dev/null
   cd conformance
-  git checkout develop
+  git checkout develop > /dev/null
   cd src
-  npm install -g grunt-cli
-  npm install
-  grunt --config="testing.config.json"
+  npm install -g grunt-cli > /dev/null
+  npm install > /dev/null
+  grunt --config="testing.config.json" > /dev/null
 
   # Stops the server.
   ps aux | grep [p]hp | awk '{print $1}' | xargs kill
