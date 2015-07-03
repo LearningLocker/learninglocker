a35bd55648f8b4e7f112385112f73d55987a3f39
Ryan Smith <0ryansmith1994@gmail.com>
2015-07-03 14:20:00 +0100

    Adds debugging to Travis.

--- test.sh
+++ test.sh
@@ -23,7 +23,9 @@ if [ "${DEVELOP}" = "${TRAVIS_BRANCH}" ]; then
   grunt --bail --config="testing.config.json" --reporter="dot" --endpoint="http://localhost:8080/data/xAPI/" --username="1484c2ac05269b8c5479a1dd6a0d6370991fd6a1" --password="f0ef3d8062805c0fc1675beb8ac0715c75df13cb" --xapi-version="1.0.1"
 
   # Stops the server.
+  echo "Stopping the server."
   ps aux | grep [p]hp | awk '{print $2}' | xargs kill
+  echo "Stopped the server."
 
   echo "Completed conformance tests."
 else
