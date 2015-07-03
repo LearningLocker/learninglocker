a90543500c162001cca06ab7dabd0725286e2b1a
Ryan Smith <0ryansmith1994@gmail.com>
2015-07-03 11:35:40 +0100

    Uses https clone url.

--- test.sh
+++ test.sh
@@ -14,7 +14,7 @@ if [ "${DEVELOP}" = "${TRAVIS_BRANCH}" ]; then
   mongo lltest --eval 'db.client.insert({"api" : { "basic_key" : "1484c2ac05269b8c5479a1dd6a0d6370991fd6a1", "basic_secret" : "f0ef3d8062805c0fc1675beb8ac0715c75df13cb" }, "lrs_id" : db.lrs.find()[0]._id, "authority" : { "name" : "New Client", "mbox" : "mailto:hello@learninglocker.net" }, "scopes" : [ "all" ]})'
 
   # Runs the test suite.
-  git clone git@github.com:ryansmith94/xAPI_LRS_Test.git conformance
+  git clone https://github.com/ryansmith94/xAPI_LRS_Test.git conformance
   cd conformance
   git checkout develop
   cd src
