56f1d1b7f80b2837f6054f58b0603ecc7802953d
Patrick Neschkudla <neschkudla@gmail.com>
2015-07-17 14:41:31 +0200

    Fixed filtering of statements with "between" values

--- app/models/Report.php
+++ app/models/Report.php
@@ -56,7 +56,14 @@ class Report extends Eloquent {
           $instructorMatch['$or'][] = $this->constructOr($key, $value);
         } else {
           if (is_array($value)) {
-            $match[$key] = ['$in' => $value];
+            if($value[0] != '<>')
+            {
+              $match[$key] = ['$in' => $value];
+            }
+            else
+            {
+              $match[$key] = ['$lte' => $value[2], '$gte' => $value[1]];
+            }
           } else {
             $match[$key] = $value;
           }
@@ -100,7 +107,14 @@ class Report extends Eloquent {
           array_push($instructorArray, [$key, $query[$key]]);
         } else {
           if (is_array($query[$key])) {
-            array_push($wheres, [$key, 'in', $query[$key]]);
+            if($query[$key][0] != '<>')
+            {
+              array_push($wheres, [$key, 'in', $query[$key]]);
+            }
+            else
+            {
+              array_push($wheres, [$key, 'between', $query[$key][1], $query[$key][2]]);
+            }
           } else {
             array_push($wheres, [$key, '=', $query[$key]]);
           }
