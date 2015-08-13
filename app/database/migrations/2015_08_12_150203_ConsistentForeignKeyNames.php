8ae721a15d6401b5086d7cd01cd7f28e322e1b70
Ryan Smith <0ryansmith1994@gmail.com>
2015-08-13 16:03:06 +0100

    Adds rollback and refactors migration.

--- app/database/migrations/2015_08_12_150203_ConsistentForeignKeyNames.php
+++ app/database/migrations/2015_08_12_150203_ConsistentForeignKeyNames.php
@@ -13,32 +13,57 @@ class ConsistentForeignKeyNames extends Migration {
 	}
 
   public function down() {
-  }
-
-  private function renameKeyOnModel($model, $old_name, $new_name) {
-    $value = array_reduce(explode('.', $old_name), function ($value, $key) {
-      return is_object($value) ? $value->{$key} : $value[$key];
-    }, $model);
-    $model->$new_name = new \MongoId($value);
-    //$model->$new_name = $value;
-    $model->save();
-  }
-
-  private function renameKeyOnModels($models, $old_name, $new_name) {
-    foreach ($models as $model){
-      $this->renameKeyOnModel($model, $old_name, $new_name);
-    }
-    echo count($models) . ' converted.'.PHP_EOL;
+    $this->chunkMongoIdRemoval(new DocumentAPI, 'lrs', 'lrs_id');
+    $this->chunkMongoIdRemoval(new Statement, 'lrs._id', 'lrs_id');
+    $this->chunkMongoIdRemoval(new Lrs, 'owner._id', 'owner_id');
+    $this->chunkMongoIdRemoval(new Report, 'lrs', 'lrs_id');
+    $this->chunkMongoIdRemoval(new Export, 'lrs', 'lrs_id');
   }
 
   private function chunkKeyRenaming($model, $old_name, $new_name) {
-    $this->chunkModelMigration($model, function($documents) use ($old_name, $new_name) {
-      $this->renameKeyOnModels($documents, $old_name, $new_name);
-    });
+    $this->chunkModelMigration($model, $this->renameKey($old_name, $new_name));
+  }
+
+  private function chunkMongoIdRemoval($model, $old_name, $new_name) {
+    $this->chunkModelMigration($model, $this->removeMongoId($old_name, $new_name));
+  }
+
+  private function removeMongoId($old_name, $new_name) {
+    return function ($model) use ($old_name, $new_name) {
+      $value = $model->$new_name;
+      $model = $this->setKey($model, explode('.', $old_name), 0, (string) $value);
+      $model->save();
+    };
+  }
+
+  private function setKey($model, $keys, $key_index, $value) {
+    if ($key_index < count($keys) - 1) {
+      $model->{$keys[$key_index]} = (object) [];
+      $this->setKey($model->{$keys[$key_index]}, $keys, $key_index + 1, $value);
+    } else {
+      $model->{$keys[$key_index]} = (string) $value;
+    }
+    return $model;
+  }
+
+  private function renameKey($old_name, $new_name) {
+    return function ($model) use ($old_name, $new_name) {
+      $value = array_reduce(explode('.', $old_name), function ($value, $key) {
+        return is_object($value) ? $value->{$key} : $value[$key];
+      }, $model);
+      $model->$new_name = new \MongoId($value);
+      $model->save();
+    };
   }
 
   private function chunkModelMigration($model, Callable $migration) {
-    $model->chunk(1000, $migration);
+    $model->chunk(1000, function ($models) use ($migration) {
+      foreach ($models as $model){
+        $migration($model);
+      }
+      echo count($models) . ' converted.'.PHP_EOL;
+    });
+
     echo 'All finished, hopefully!'.PHP_EOL;
   }
 
