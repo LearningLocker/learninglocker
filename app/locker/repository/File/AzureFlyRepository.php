b7a8e049845e320665149a77036627b7e49b3ada
Ryan Smith <0ryansmith1994@gmail.com>
2015-08-21 13:06:33 +0100

    Corrects Azure support.

--- app/locker/repository/File/AzureFlyRepository.php
+++ app/locker/repository/File/AzureFlyRepository.php
@@ -1,8 +1,8 @@
 <?php namespace Locker\Repository\File;
-use OpenCloud\Rackspace as Rackspace;
-use League\Flysystem\Rackspace\RackspaceAdapter as RackspaceAdapter;
+use WindowsAzure\Common\ServicesBuilder;
+use League\Flysystem\Azure\AzureAdapter;
 
-class RackspaceFlyRepository extends FlyRepository {
+class AzureFlyRepository extends FlyRepository {
 
   public function __construct(Callable $conf) {
     $endpoint = sprintf(
@@ -10,10 +10,9 @@ class RackspaceFlyRepository extends FlyRepository {
         $conf('FS_AZURE_USERNAME'),
         conf('FS_AZURE_API_KEY')
     );
-
     $blobRestProxy = ServicesBuilder::getInstance()->createBlobService($endpoint);
-
-    $filesystem = new Filesystem(new AzureAdapter($blobRestProxy, conf('FS_AZURE_CONTAINER')));
+    $adapter = new AzureAdapter($blobRestProxy, conf('FS_AZURE_CONTAINER'));
+    $this->constructFileSystem($adapter);
   }
 
 }
