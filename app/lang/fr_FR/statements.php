e4afef4f207b0c27cf6b714814eef49e8692f7dd
Yannick Warnier <ywarnier@beeznest.org>
2017-08-01 22:43:12 +0200

    Add French translation

---
+++ app/lang/fr_FR/statements.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|----------------------------------------------------------------------------
+| Translations for statements
+|----------------------------------------------------------------------------
+*/
+
+return array(
+    'statements' => 'Statements',
+    'generator'  => 'Générateur',
+    'filter'	 => 'filtre',
+    'explorer'   => 'Explorer',
+    'reporting'  => 'Rapports',
+    'exporting'  => 'Exports',
+    'analytics'  => 'Analytique',
+    'added'		 => 'Votre statement a été ajouté.'
+);
