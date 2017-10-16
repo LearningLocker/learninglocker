f70f8087063a2b859e56c7c4cd91fb632503f849
Chris Bishop <chrisjbishop155@gmail.com>
2017-10-16 15:02:00 +0100

    Fixed user tests.

--- api/src/routes/tests/scopeFiltering/visualisations/delete-test.js
+++ api/src/routes/tests/scopeFiltering/visualisations/delete-test.js
@@ -25,108 +25,131 @@ describe('API HTTP DELETE visualisations route scope filtering', () => {
   const assertVisualisation = async({
     isPublic,
     isOwner,
-    token,
+    bearerToken,
+    basicToken,
     expectedStatus = 204
   }) => {
     const visualisation = await createVisualisation(isPublic,
       isOwner ? TEST_USER_ID : objectId());
 
+    const authorization = bearerToken ?
+      `Bearer ${bearerToken}` :
+        `Basic ${basicToken}`;
+
     return apiApp
       .delete(`${RESTIFY_PREFIX}/visualisation/${visualisation._id.toString()}`)
-      .set('Authorization', `Bearer ${token}`)
+      .set('Authorization', authorization)
       .set('Content-Type', 'application/json')
       .expect(expectedStatus);
   };
 
   const assertPublicVisualisation = async ({
-    token,
+    bearerToken,
+    basicToken,
     expectedStatus = 204
   }) =>
     assertVisualisation({
       isPublic: true,
       isOwner: false,
-      token,
+      bearerToken,
+      basicToken,
       expectedStatus
     });
 
-  const assertMyPublicVisualisation = async ({
+  const assertMyBearerPublicVisualisation = async ({
     token,
     expectedStatus = 204
   }) =>
     assertVisualisation({
       isPublic: true,
       isOwner: true,
-      token,
+      bearerToken: token,
       expectedStatus
     });
 
   const assertPrivateVisualisation = async ({
-    token,
+    bearerToken,
+    basicToken,
     expectedStatus = 204
   }) =>
     assertVisualisation({
       isPublic: false,
       isOwner: false,
-      token,
+      bearerToken,
+      basicToken,
       expectedStatus
     });
 
-  const assertMyPrivateVisualisation = async ({
+  const assertMyBearerPrivateVisualisation = async ({
     token,
     expectedStatus = 204
   }) =>
     assertVisualisation({
       isPublic: false,
       isOwner: true,
-      token,
+      bearerToken: token,
       expectedStatus
     });
 
+
+
   it('should delete inside the org when using all scope', async () => {
     const token = await createOrgToken([ALL], [], TEST_USER_ID);
-    await assertPublicVisualisation({ token, expectedStatus: 204 });
-    await assertPrivateVisualisation({ token, expectedStatus: 204 });
-    await assertMyPrivateVisualisation({ token, expectedStatus: 204 });
-    await assertMyPublicVisualisation({ token, expectedStatus: 204 });
+    await assertPublicVisualisation({ bearerToken: token, expectedStatus: 204 });
+    await assertPrivateVisualisation({ bearerToken: token, expectedStatus: 204 });
+    await assertMyBearerPrivateVisualisation({ token, expectedStatus: 204 });
+    await assertMyBearerPublicVisualisation({ token, expectedStatus: 204 });
   });
 
   it('should delete inside the org when using no scopes', async () => {
     const token = await createOrgToken([], [], TEST_USER_ID);
-    await assertPublicVisualisation({ token, expectedStatus: 404 });
-    await assertPrivateVisualisation({ token, expectedStatus: 404 });
-    await assertMyPublicVisualisation({ token, expectedStatus: 204 });
-    await assertMyPrivateVisualisation({ token, expectedStatus: 204 });
+    await assertPublicVisualisation({ bearerToken: token, expectedStatus: 404 });
+    await assertPrivateVisualisation({ bearerToken: token, expectedStatus: 404 });
+    await assertMyBearerPublicVisualisation({ token, expectedStatus: 204 });
+    await assertMyBearerPrivateVisualisation({ token, expectedStatus: 204 });
   });
 
   it('should delete inside the org when using edit all scope', async () => {
     const token = await createOrgToken([EDIT_ALL_VISUALISATIONS], [], TEST_USER_ID);
-    await assertPublicVisualisation({ token, expectedStatus: 204 });
-    await assertPrivateVisualisation({ token, expectedStatus: 204 });
-    await assertMyPublicVisualisation({ token, expectedStatus: 204 });
-    await assertMyPrivateVisualisation({ token, expectedStatus: 204 });
+    await assertPublicVisualisation({ bearerToken: token, expectedStatus: 204 });
+    await assertPrivateVisualisation({ bearerToken: token, expectedStatus: 204 });
+    await assertMyBearerPublicVisualisation({ token, expectedStatus: 204 });
+    await assertMyBearerPrivateVisualisation({ token, expectedStatus: 204 });
   });
 
   it('should delete inside the org when using edit public scope', async () => {
     const token = await createOrgToken([EDIT_PUBLIC_VISUALISATIONS], [], TEST_USER_ID);
-    await assertPublicVisualisation({ token, expectedStatus: 204 });
-    await assertPrivateVisualisation({ token, expectedStatus: 404 });
-    await assertMyPublicVisualisation({ token, expectedStatus: 204 });
-    await assertMyPrivateVisualisation({ token, expectedStatus: 204 });
+    await assertPublicVisualisation({ bearerToken: token, expectedStatus: 204 });
+    await assertPrivateVisualisation({ bearerToken: token, expectedStatus: 404 });
+    await assertMyBearerPublicVisualisation({ token, expectedStatus: 204 });
+    await assertMyBearerPrivateVisualisation({ token, expectedStatus: 204 });
   });
 
   it('should delete inside the org when using owner org token', async () => {
     const token = await createOwnerOrgToken(TEST_USER_ID);
-    await assertPublicVisualisation({ token, expectedStatus: 404 });
-    await assertPrivateVisualisation({ token, expectedStatus: 404 });
-    await assertMyPublicVisualisation({ token, expectedStatus: 204 });
-    await assertMyPrivateVisualisation({ token, expectedStatus: 204 });
+    await assertPublicVisualisation({ bearerToken: token, expectedStatus: 404 });
+    await assertPrivateVisualisation({ bearerToken: token, expectedStatus: 404 });
+    await assertMyBearerPublicVisualisation({ token, expectedStatus: 204 });
+    await assertMyBearerPrivateVisualisation({ token, expectedStatus: 204 });
   });
 
   it('should delete inside the org when using site admin token', async () => {
     const token = await createUserToken([SITE_ADMIN], TEST_USER_ID);
-    await assertPublicVisualisation({ token, expectedStatus: 204 });
-    await assertPrivateVisualisation({ token, expectedStatus: 204 });
-    await assertMyPublicVisualisation({ token, expectedStatus: 204 });
-    await assertMyPrivateVisualisation({ token, expectedStatus: 204 });
+    await assertPublicVisualisation({ bearerToken: token, expectedStatus: 204 });
+    await assertPrivateVisualisation({ bearerToken: token, expectedStatus: 204 });
+    await assertMyBearerPublicVisualisation({ token, expectedStatus: 204 });
+    await assertMyBearerPrivateVisualisation({ token, expectedStatus: 204 });
   });
+
+  // it('should delete when client basic has the ALL scope', async () => {
+  //   const token = ?
+  //   await assertPublicVisualisation({ basicToken: token, expectedStatus: 204 });
+  //   await assertPrivateVisualisation({ basicToken: token, expectedStatus: 204 });
+  // });
+
+  // it('should delete when client basic has no scopes', async () => {
+  //   const token = ?
+  //   await assertPublicVisualisation({ basicToken: token, expectedStatus: 404 });
+  //   await assertPrivateVisualisation({ basicToken: token, expectedStatus: 404 });
+  // });
 });
