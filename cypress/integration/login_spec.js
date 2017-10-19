3a4a6912f00a2f4529100faf7f603ba00ca33358
Chris Bishop <chrisjbishop155@gmail.com>
2017-10-19 15:27:42 +0100

    adds org login test

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -2,18 +2,23 @@ describe.only('log in', () => {
   it('should log in', () => {
     cy.log('001.1');
 
-    // cy.exec('node --version');
-
-    cy.log('001.2');
-
-    cy.exec('RUNTIME_NODE_ENV=test node cli/dist/server seed reset', {
-      // env: {
-      //   RUNTIME_NODE_ENV: 'test'
-      // },
+    // For some reason we need to call this first otherwise the seed command times out ???
+    cy.exec('node --version', {
       log: true
+    }).then((result) => {
+      cy.log('node version', result.stdout);
     });
 
-    cy.log('002');
+
+    cy.exec('node cli/dist/server seed reset', {
+      env: {
+        RUNTIME_NODE_ENV: 'test'
+      },
+      log: true
+    }).then(() => {
+      cy.log('seed added succesfully');
+    });
+
     cy.visit('http://localhost:3000');
 
     cy.get('#containersLoginEmail')
@@ -27,5 +32,8 @@ describe.only('log in', () => {
 
     cy.location('pathname').should('include', 'home');
 
+    cy.contains('Test organisation').click();
+
+    cy.location('pathname').should('include', 'organisation');
   });
 });
