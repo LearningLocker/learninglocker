9520893b75fcd87b0bcc16484f872fe54014b818
Andrew Hickey <andrewhickey@live.co.uk>
2017-10-24 11:06:41 +0100

    moves getToken seed to separate function

--- cli/src/seed/index.js
+++ cli/src/seed/index.js
@@ -16,6 +16,33 @@ const resetState = async () => {
   logger.info('Reset success');
 };
 
+const getToken = async () => {
+  const user = await User.findOne({
+    email: 'chris.bishop@ht2labs.com'
+  });
+
+  const organisation = await Organisation.findOne({
+    name: 'Test organisation'
+  });
+
+  const orgCookieName = getCookieName({ tokenType: 'organisation', tokenId: organisation._id });
+  const orgJwtToken = await createOrgJWT(user, organisation._id, 'native');
+
+  const userCookieName = getCookieName({ tokenType: 'user', tokenId: user._id });
+  const userJwtToken = await createUserJWT(user, 'native');
+
+  const cookies = {
+    [orgCookieName]: orgJwtToken,
+    [userCookieName]: userJwtToken
+  };
+
+  process.stdout.write(JSON.stringify({
+    cookies,
+    organisationId: organisation._id,
+    userId: user._id
+  }));
+};
+
 // RUNTIME_NODE_ENV=test node cli/dist/server seed reset
 export default async function (action) {
   try {
@@ -30,33 +57,8 @@ export default async function (action) {
         break;
       }
       case 'getToken': {
-        const user = await User.findOne({
-          email: 'chris.bishop@ht2labs.com'
-        });
-
-        const organisation = await Organisation.findOne({
-          name: 'Test organisation'
-        });
-
-        const orgCookieName = getCookieName({ tokenType: 'organisation', tokenId: organisation._id });
-        const orgJwtToken = await createOrgJWT(user, organisation._id, 'native');
-
-        const userCookieName = getCookieName({ tokenType: 'user', tokenId: user._id });
-        const userJwtToken = await createUserJWT(user, 'native');
-
-        const cookies = {
-          [orgCookieName]: orgJwtToken,
-          [userCookieName]: userJwtToken
-        };
-
-        // const orgCookieString = `${userCookieName}=${userJwtToken}; ${orgCookieName}=${orgJwtToken}`;
-
-        process.stdout.write(JSON.stringify({
-          cookies,
-          organisationId: organisation._id,
-          userId: user._id
-        }));
-
+        logger.info('Getting auth token');
+        await getToken();
         break;
       }
       default: {
