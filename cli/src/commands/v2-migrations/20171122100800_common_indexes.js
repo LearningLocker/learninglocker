53adf6f82c7f774db9be1680240606185f264045
Ryan Smith <0ryansmith1994@gmail.com>
2017-11-22 14:51:11 +0000

    Adds index names.

--- cli/src/commands/v2-migrations/20171122100800_common_indexes.js
+++ cli/src/commands/v2-migrations/20171122100800_common_indexes.js
@@ -4,6 +4,7 @@ import { getConnection } from 'lib/connections/mongoose';
 import logger from 'lib/logger';
 
 const createIndex = (collection, name, index, opts = {}) => {
+  logger.silly(`Creating ${name} index`, index);
   return collection.createIndex(index, { name, background: true, ...opts });
 };
 
@@ -17,40 +18,58 @@ const createIndexes = (collection, indexes) => {
   });
 };
 
-const orgIndex = { organisation: 1 };
-const storeIndex = { ...orgIndex, lrs_id: 1 };
-const voidIndex = { ...storeIndex, voided: 1 };
-const objectIdIndex = { 'statement.object.id': 1 };
-const verbIdIndex = { 'statement.verb.id': 1 };
-const objectTypeIndex = { 'statement.object.objectType': 1 };
-const accountIndex = { 'statement.actor.account.name': 1, 'statement.actor.account.homePage': 1 };
+const createStatementIndexes = connection => {
+  const stmts = connection.collection('statements');
+
+  const objectIdIndex = { 'statement.object.id': 1 };
+  const verbIdIndex = { 'statement.verb.id': 1 };
+  const objectTypeIndex = { 'statement.object.objectType': 1 };
+  const accountIndex = { 'statement.actor.account.name': 1, 'statement.actor.account.homePage': 1 };
+
+  const createIndexWithOrg = (name, index) => {
+    return createIndex(stmts, `org_${name}`, { organisation: 1, ...index });
+  };
+  const createIndexWithStore = (name, index) => {
+    return createIndexWithOrg(`lrs_${name}`, { lrs_id: 1, ...index });
+  };
+  const createIndexWithVoid = (name, index) => {
+    return createIndexWithStore(`voided_${name}`, { voided: 1, ...index });
+  };
+
+  createIndexWithOrg('objId_objType', { ...objectIdIndex, ...objectTypeIndex });
+  createIndexWithVoid('verbId_objType', { ...verbIdIndex, ...objectTypeIndex });
+  createIndexWithVoid('verbId_objId', { ...verbIdIndex, ...objectIdIndex });
+  createIndexWithVoid('mbox', { 'statement.actor.mbox': 1 });
+  createIndexWithVoid('account', accountIndex);
+  createIndexWithVoid('timestamp_id', { timestamp: -1, _id: -1 });
+  createIndexWithVoid('stored_id', { stored: -1, _id: -1 });
+  createIndexWithStore('statementId', { 'statement.id': 1 });
+  createIndexWithOrg('timestamp_id', { timestamp: -1, _id: 1 });
+  createIndexWithOrg('stored_id', { stored: -1, _id: 1 });
+  createIndexWithOrg('objId', objectIdIndex);
+  createIndexWithOrg('verbId_objId', { ...verbIdIndex, ...objectIdIndex });
+  createIndexWithOrg('ident', { personaIdentifier: 1 });
+  createIndexWithOrg('person_timestamp', { 'person._id': 1, timestamp: -1 });
+  createIndexWithOrg('voided', { voided: 1 });
+  createUnique(stmts, 'org_lrs_hash', { organisation: 1, lrs_id: 1, hash: 1 });
+};
+
+const createClientIndexes = connection => {
+  const clients = connection.collection('client');
+  createUnique(clients, 'apiKey_apiSecret', { 'api.basic_key': 1, 'api.basic_secret': 1 });
+};
+
+const createActivityIndexes = connection => {
+  const fullActivities = connection.collection('fullActivities');
+  const activityIndex = { organisationId: 1, lrsId: 1, activityId: 1 };
+  createUnique(fullActivities, 'orgId_lrsId_activityId', activityIndex);
+};
 
 const up = async () => {
   const connection = getConnection();
-
-  const stmts = connection.collection('statements');
-  createIndex(stmts, 'object', { ...storeIndex, ...objectIdIndex, ...objectTypeIndex });
-  createIndex(stmts, 'verb_object_type', { ...voidIndex, ...verbIdIndex, ...objectTypeIndex });
-  createIndex(stmts, 'verb_object_id', { ...voidIndex, ...verbIdIndex, ...objectIdIndex });
-  createIndex(stmts, 'mbox', { ...voidIndex, 'statement.actor.mbox': 1 });
-  createIndex(stmts, 'account', { ...voidIndex, ...accountIndex });
-  createIndex(stmts, 'timestamp', { ...voidIndex, timestamp: -1, _id: -1 });
-  createIndex(stmts, 'stored', { ...voidIndex, stored: -1, _id: -1 });
-  createIndex(stmts, 'statement_id', { ...storeIndex, 'statement.id': 1 });
-  createIndex(stmts, 'timestamp_in_org', { ...orgIndex, timestamp: -1, _id: 1 });
-  createIndex(stmts, 'stored_in_org', { ...orgIndex, stored: -1, _id: 1 });
-  createIndex(stmts, 'object_in_org', { ...orgIndex, ...objectIdIndex });
-  createIndex(stmts, 'verb_object_in_org', { ...orgIndex, ...verbIdIndex, ...objectIdIndex });
-  createIndex(stmts, 'personaIdentifier', { ...orgIndex, personaIdentifier: 1 });
-  createIndex(stmts, 'person', { ...orgIndex, 'person._id': 1, timestamp: -1 });
-  createIndex(stmts, 'voided', { ...orgIndex, voided: 1 });
-  createUnique(stmts, 'hash', { ...storeIndex, hash: 1 });
-
-  const clients = connection.collection('client');
-  createUnique(clients, 'key_secret', { 'api.basic_key': 1, 'api.basic_secret': 1 });
-
-  const fullActivities = connection.collection('fullActivities');
-  createUnique(fullActivities, 'activityId', { organisationId: 1, lrsId: 1, activityId: 1 });
+  createStatementIndexes(connection);
+  createClientIndexes(connection);
+  createActivityIndexes(connection);
 };
 
 const down = async () => {
