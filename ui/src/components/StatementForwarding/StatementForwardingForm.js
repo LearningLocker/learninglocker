20cbffdc008eea7a9650a9249f079687b5326b7b
James Mullaney <james@ht2.co.uk>
2017-11-24 17:28:42 +0000

    style(lint): Various linting

--- ui/src/components/StatementForwarding/StatementForwardingForm.js
+++ ui/src/components/StatementForwarding/StatementForwardingForm.js
@@ -114,101 +114,100 @@ const StatementForwardingForm = ({
   authTypes,
   changeMaxRetries,
   changeHeaders
-}) => {
-  return (
-    <div>
-      <div className="row">
-        <div className="col-md-12">
+}) => (
+  <div>
+    <div className="row">
+      <div className="col-md-12">
 
-          <div className="form-group">
-            <Switch
-              label="Active?"
-              onChange={changeActive}
-              checked={model.get('active', false)} />
-          </div>
+        <div className="form-group">
+          <Switch
+            label="Active?"
+            onChange={changeActive}
+            checked={model.get('active', false)} />
+        </div>
 
-          <div className="form-group">
-            <label htmlFor={`${model.get('_id')}descriptionInput`}>Name</label>
-            <input
-              id={`${model.get('_id')}descriptionInput`}
-              className="form-control"
-              placeholder="Short description of this statement forwarder"
-              value={model.get('description', '')}
-              onChange={changeDescription} />
-          </div>
-          <div className="form-group">
-            <label htmlFor={`${model.get('_id')}protocolInput`}>Protocol</label>
-            <select
-              className="form-control"
-              onChange={changeProtocol}
-              value={model.getIn(['configuration', 'protocol'], 'http')} >
-              {protocols.map(protocol => (
-                <option key={protocol} value={protocol}>{protocol}</option>
+        <div className="form-group">
+          <label htmlFor={`${model.get('_id')}descriptionInput`}>Name</label>
+          <input
+            id={`${model.get('_id')}descriptionInput`}
+            className="form-control"
+            placeholder="Short description of this statement forwarder"
+            value={model.get('description', '')}
+            onChange={changeDescription} />
+        </div>
+        <div className="form-group">
+          <label htmlFor={`${model.get('_id')}protocolInput`}>Protocol</label>
+          <select
+            className="form-control"
+            onChange={changeProtocol}
+            value={model.getIn(['configuration', 'protocol'], 'http')} >
+            {protocols.map(protocol => (
+              <option key={protocol} value={protocol}>{protocol}</option>
               ))}
-            </select>
-          </div>
-          <div
-            className={classNames({
-              'form-group': true,
-              'has-error': model.getIn(['errors', 'messages', 'configuration.url'], false)
-            })} >
-            <label htmlFor={`${model.get('_id')}urlInput`}>URL</label>
-            <input
-              id={`${model.get('_id')}urlInput`}
-              className="form-control"
-              value={model.getIn(['configuration', 'url'], '')}
-              placeholder="URL"
-              onChange={changeUrl} />
-            { model.getIn(['errors', 'messages', 'configuration.url'], false) &&
+          </select>
+        </div>
+        <div
+          className={classNames({
+            'form-group': true,
+            'has-error': model.getIn(['errors', 'messages', 'configuration.url'], false)
+          })} >
+          <label htmlFor={`${model.get('_id')}urlInput`}>URL</label>
+          <input
+            id={`${model.get('_id')}urlInput`}
+            className="form-control"
+            value={model.getIn(['configuration', 'url'], '')}
+            placeholder="URL"
+            onChange={changeUrl} />
+          { model.getIn(['errors', 'messages', 'configuration.url'], false) &&
               (<span className="help-block">
                 <ValidationList errors={model.getIn(['errors', 'messages', 'configuration.url'])} />
               </span>)
             }
-          </div>
+        </div>
 
-          <div
-            className={classNames({
-              'form-group': true,
-              'has-error': model.getIn(['errors', 'messages', 'configuration.authType'], false)
-            })}>
-            <label htmlFor={`${model.get('_id')}authTypeInput`}>Auth Type</label>
-            <select
-              className="form-control"
-              onChange={changeAuthType}
-              value={model.getIn(['configuration', 'authType'], AUTH_TYPE_NO_AUTH)}>
-              {authTypes.map(authType => (
-                <option key={authType} value={authType}>{authType}</option>
+        <div
+          className={classNames({
+            'form-group': true,
+            'has-error': model.getIn(['errors', 'messages', 'configuration.authType'], false)
+          })}>
+          <label htmlFor={`${model.get('_id')}authTypeInput`}>Auth Type</label>
+          <select
+            className="form-control"
+            onChange={changeAuthType}
+            value={model.getIn(['configuration', 'authType'], AUTH_TYPE_NO_AUTH)}>
+            {authTypes.map(authType => (
+              <option key={authType} value={authType}>{authType}</option>
               ))}
-            </select>
-            { model.getIn(['errors', 'messages', 'configuration.authType'], false) &&
+          </select>
+          { model.getIn(['errors', 'messages', 'configuration.authType'], false) &&
               (<span className="help-block">
                 <ValidationList errors={model.getIn(['errors', 'messages', 'configuration.authType'])} />
               </span>)
             }
-          </div>
+        </div>
 
-          {(model.getIn(['configuration', 'authType'], 'token') === 'token') &&
-            <div
-              className={classNames({
-                'form-group': true,
-                'has-error': model.getIn(['errors', 'messages', 'configuration.secret'], false)
-              })}>
-              <label htmlFor={`${model.get('_id')}secretInput`}>Secret Key</label>
-              <input
-                id={`${model.getIn(['configuration', 'secret'])}secretInput`}
-                className="form-control"
-                value={model.getIn(['configuration', 'secret'], '')}
-                placeholder="Secret Key"
-                onChange={changeSecret} />
-              { model.getIn(['errors', 'messages', 'configuration.secret'], false) &&
+        {(model.getIn(['configuration', 'authType'], 'token') === 'token') &&
+        <div
+          className={classNames({
+            'form-group': true,
+            'has-error': model.getIn(['errors', 'messages', 'configuration.secret'], false)
+          })}>
+          <label htmlFor={`${model.get('_id')}secretInput`}>Secret Key</label>
+          <input
+            id={`${model.getIn(['configuration', 'secret'])}secretInput`}
+            className="form-control"
+            value={model.getIn(['configuration', 'secret'], '')}
+            placeholder="Secret Key"
+            onChange={changeSecret} />
+          { model.getIn(['errors', 'messages', 'configuration.secret'], false) &&
                 (<span className="help-block">
                   <ValidationList errors={model.getIn(['errors', 'messages', 'configuration.secret'])} />
                 </span>)
               }
-            </div>
+        </div>
           }
 
-          {(model.getIn(['configuration', 'authType'], 'token') === 'basic auth') &&
+        {(model.getIn(['configuration', 'authType'], 'token') === 'basic auth') &&
             (<div>
               <div
                 className={classNames({
@@ -249,65 +248,64 @@ const StatementForwardingForm = ({
             </div>)
           }
 
-          <div
-            className={classNames({
-              'form-group': true,
-              'has-error': model.getIn(['errors', 'messages', 'configuration.maxRetries'], false)
-            })}>
-            <label htmlFor={`${model.get('_id')}maxRetries`}>{`Max Retries (0 - ${STATAMENT_FORWARDING_MAX_RETRIES})`}</label>
-            <input
-              id={`${model.getIn(['configuration', 'maxRetries'], 3)}maxRetries`}
-              type="number"
-              step="1"
-              min="0"
-              max={STATAMENT_FORWARDING_MAX_RETRIES}
-              className="form-control"
-              value={model.getIn(['configuration', 'maxRetries'], 3)}
-              placeholder="3"
-              onChange={changeMaxRetries} />
-            { model.getIn(['errors', 'messages', 'configuration.maxRetries'], false) &&
+        <div
+          className={classNames({
+            'form-group': true,
+            'has-error': model.getIn(['errors', 'messages', 'configuration.maxRetries'], false)
+          })}>
+          <label htmlFor={`${model.get('_id')}maxRetries`}>{`Max Retries (0 - ${STATAMENT_FORWARDING_MAX_RETRIES})`}</label>
+          <input
+            id={`${model.getIn(['configuration', 'maxRetries'], 3)}maxRetries`}
+            type="number"
+            step="1"
+            min="0"
+            max={STATAMENT_FORWARDING_MAX_RETRIES}
+            className="form-control"
+            value={model.getIn(['configuration', 'maxRetries'], 3)}
+            placeholder="3"
+            onChange={changeMaxRetries} />
+          { model.getIn(['errors', 'messages', 'configuration.maxRetries'], false) &&
               (<span className="help-block">
                 <ValidationList errors={model.getIn(['errors', 'messages', 'configuration.maxRetries'])} />
               </span>
               )}
-          </div>
+        </div>
 
-          <div
-            className={classNames({
-              'form-group': true,
-              'has-error': model.getIn(['errors', 'messages', 'configuration.headers'], false)
-            })}>
-            <label htmlFor={`${model.get('_id')}headers`}>Headers</label>
-            <TableInput
-              keyName="Field Name"
-              valueName="Field Value"
-              staticValues={getAuthHeaders({
-                configuration: model.get('configuration').toJS()
-              }).merge(new Map({
-                'Content-Type': 'application/json',
-                'Content-Length': '${Content-Length}' // eslint-disable-line no-template-curly-in-string
-              }))}
-              values={fromJS(JSON.parse(model.getIn(['configuration', 'headers'], '{}')))}
-              onChange={changeHeaders} />
-            {
+        <div
+          className={classNames({
+            'form-group': true,
+            'has-error': model.getIn(['errors', 'messages', 'configuration.headers'], false)
+          })}>
+          <label htmlFor={`${model.get('_id')}headers`}>Headers</label>
+          <TableInput
+            keyName="Field Name"
+            valueName="Field Value"
+            staticValues={getAuthHeaders({
+              configuration: model.get('configuration').toJS()
+            }).merge(new Map({
+              'Content-Type': 'application/json',
+              'Content-Length': '${Content-Length}' // eslint-disable-line no-template-curly-in-string
+            }))}
+            values={fromJS(JSON.parse(model.getIn(['configuration', 'headers'], '{}')))}
+            onChange={changeHeaders} />
+          {
               model.getIn(['errors', 'messages', 'configuration.headers'], false) &&
               (<span className="help-block">
                 <ValidationList errors={model.getIn(['errors', 'messages', 'configuration.headers'])} />
               </span>)
             }
-          </div>
+        </div>
 
-          <div className="form-group">
-            <QueryBuilder
-              componentPath={new List(['statementForwarding', model.get('_id')])}
-              query={model.get('query')}
-              onChange={changeQuery} />
-          </div>
+        <div className="form-group">
+          <QueryBuilder
+            componentPath={new List(['statementForwarding', model.get('_id')])}
+            query={model.get('query')}
+            onChange={changeQuery} />
         </div>
       </div>
     </div>
+  </div>
   );
-};
 
 export default compose(
   withProps(props => ({
