17d7d969f40cab274502344ed05f52b06fc567e9
Ryan Smith <0ryansmith1994@gmail.com>
2017-11-27 22:40:01 +0000

    ci(travis): Fixes build errors.

--- cli/src/commands/v2-migrations/index.js
+++ cli/src/commands/v2-migrations/index.js
@@ -1,6 +1,6 @@
 import { OrderedMap } from 'immutable';
-import migratePersonas from '20171127214500_migrate_personas';
-import migrateIdentifiers from '20171127214900_migrate_identifiers';
+import migratePersonas from './20171127214500_migrate_personas';
+import migrateIdentifiers from './20171127214900_migrate_identifiers';
 
 export default new OrderedMap()
   .set('20171127214500_migrate_personas', migratePersonas)
