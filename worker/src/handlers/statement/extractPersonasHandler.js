ae1096f33e17e11b28d30f4bee56489a49ead654
James Mullaney <james@ht2.co.uk>
2017-12-13 16:27:28 +0000

    fix(worker): Bad file reference

--- worker/src/handlers/statement/extractPersonasHandler.js
+++ worker/src/handlers/statement/extractPersonasHandler.js
@@ -3,7 +3,7 @@ import Promise from 'bluebird';
 import wrapHandlerForStatement from 'worker/handlers/statement/wrapHandlerForStatement';
 import { STATEMENT_EXTRACT_PERSONAS_QUEUE } from 'lib/constants/statements';
 import asignIdentifierToStatements from 'lib/services/persona/asignIdentifierToStatements';
-import getIfiFromActor from 'lib/services/persona/libs/getIfiFromActor';
+import getIfiFromActor from 'lib/services/persona/utils/getIfiFromActor';
 
 const handleStatement = personaService => async (statement) => {
   const ifi = getIfiFromActor(statement.statement.actor);
