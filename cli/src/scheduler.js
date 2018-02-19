3a57d4a24f9540d331c745931796fabb2ea93cf3
Chris Bishop <chrisjbishop155@gmail.com>
2018-02-19 14:27:47 +0000

    Fixed typo.

--- cli/src/scheduler.js
+++ cli/src/scheduler.js
@@ -1,7 +1,7 @@
 import expirationNotificationEmails from 'worker/commands/expirationNotificationEmails';
 import logger from 'lib/logger';
 
-const timeout = 15 * 60 * 100;
+const timeout = 15 * 60 * 1000;
 
 const run = async () => {
   logger.info('processing expiration');
