24d4aafdf088533abdd6534e2626d87f58adbf3c
Ryan Smith <0ryansmith1994@gmail.com>
2018-02-20 16:04:52 +0000

    fix: Persona clientBasic auth.

--- api/src/routes/personas/personaIdentifierRESTHandler.js
+++ api/src/routes/personas/personaIdentifierRESTHandler.js
@@ -8,57 +8,57 @@ const router = new express.Router();
 
 router.get(
   routes.PERSONA_IDENTIFIER_COUNT,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaIdentifierController.personaIdentifierCount
 );
 router.get(
   routes.PERSONA_IDENTIFIER,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaIdentifierController.getPersonaIdentifiers
 );
 router.get(
   routes.PERSONA_IDENTIFIER_ID,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaIdentifierController.getPersonaIdentifier
 );
 router.post(
   routes.PERSONA_IDENTIFIER,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaIdentifierController.addPersonaIdentifier
 );
 router.post(
   routes.PERSONA_IDENTIFIER_UPSERT,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaIdentifierController.upsertPersonaIdentifier
 );
 router.post(
   routes.PERSONA_IDENTIFIER_ID,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaIdentifierController.updatePersonaIdentifier
 );
 router.put(
   routes.PERSONA_IDENTIFIER,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaIdentifierController.addPersonaIdentifier
 );
 router.put(
   routes.PERSONA_IDENTIFIER_ID,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaIdentifierController.updatePersonaIdentifier
 );
 router.patch(
   routes.PERSONA_IDENTIFIER_ID,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaIdentifierController.updatePersonaIdentifier
 );
 router.delete(
   routes.PERSONA_IDENTIFIER_ID,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaIdentifierController.deletePersonaIdentifier
 );
 router.get(
   routes.CONNECTION_PERSONA_IDENTIFIER,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaIdentifierController.personaIdentifierConnection
 );
 
--- api/src/routes/personas/personaRESTHandler.js
+++ api/src/routes/personas/personaRESTHandler.js
@@ -8,63 +8,63 @@ const router = new express.Router();
 
 router.get(
   routes.PERSONA_COUNT,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaController.personaCount
 );
 router.get(
   routes.PERSONA,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaController.getPersonas
 );
 router.get(
   routes.PERSONA_ID,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaController.getPersona
 );
 router.post(
   routes.PERSONA,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaController.addPersona
 );
 router.post(
   routes.PERSONA_ID,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaController.updatePersona
 );
 router.put(
   routes.PERSONA,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaController.addPersona
 );
 router.put(
   routes.PERSONA_ID,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaController.updatePersona
 );
 router.patch(
   routes.PERSONA_ID,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaController.updatePersona
 );
 router.delete(
   routes.PERSONA_ID,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaController.deletePersona
 );
 router.get(
   routes.CONNECTION_PERSONA,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaController.personaConnection
 );
 router.post(
   routes.MERGE_PERSONA,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaController.mergePersona
 );
 
 router.post(
   routes.MERGE_PERSONA,
-  passport.authenticate(['jwt', 'client_basic'], DEFAULT_PASSPORT_OPTIONS),
+  passport.authenticate(['jwt', 'clientBasic'], DEFAULT_PASSPORT_OPTIONS),
   PersonaController.mergePersona
 );
 
