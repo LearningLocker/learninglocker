1ecddb3fa3386a449d42008982f548bc6a7319f5
James Mullaney <james@ht2.co.uk>
2018-04-17 12:37:25 +0100

    fix: Show persona dates

--- ui/src/containers/Owner/index.js
+++ ui/src/containers/Owner/index.js
@@ -1,24 +1,41 @@
 import React from 'react';
+import getDateFromMongoID from 'lib/helpers/getDateFromMongoID';
 import { withProps, compose } from 'recompose';
 import { withModel } from 'ui/utils/hocs';
 import createdAtFormatter from './CreatedAt';
 
+const getCreatedDate = (createdAt, _id) => {
+  if (!createdAt && _id) {
+    try {
+      return getDateFromMongoID(_id);
+    } catch (err) {
+      // error getting date from id (might not be a valid mongo id)
+      return;
+    }
+  }
+  return createdAt;
+}
+
 const Creator = compose(
   withProps(({ model }) => ({ schema: 'user', id: model.get('owner') })),
   withModel
-)(({ model, createdAt }) => {
+)(({ model, date }) => {
   const email = model.get('email', false);
   const name = model.get('name', false);
   return (
     <div>
       <div style={{ fontWeight: 'bold' }}>Made by { email ? <i>{name || email}</i> : <i>deleted user</i> }</div>
-      <div>{ createdAtFormatter(createdAt) }</div>
+      <div>{ date }</div>
     </div>
   );
 });
 
-export default ({ model }) => (
-  model.has('owner')
-  ? <Creator model={model} createdAt={model.get('createdAt', false)} />
-  : <div style={{ marginTop: 8 }}>{ createdAtFormatter(model.get('createdAt')) }</div>
-);
+export default ({ model }) => {
+  const date = getCreatedDate(model.get('createdAt', false), model.get('_id'));
+  const formattedDate = createdAtFormatter(date);
+  return (
+    model.has('owner')
+    ? <Creator model={model} date={ formattedDate } />
+    : <div style={{ marginTop: 8 }}>{ formattedDate }</div>
+  )
+};
