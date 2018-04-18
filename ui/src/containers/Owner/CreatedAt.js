87084f6858e229c71a70cfd29c9a3db79a31f144
James Mullaney <james@ht2.co.uk>
2018-04-18 08:41:09 +0100

    fix: Use local file when importing csvs, and use clamd (#1141)
    * fix: Use local file when importing csvs
    
    * ci(lint): Remove unused import
    
    * fix: Use clamdscan for faster scanning
    
    * Update .env.example
    
    * Update .env.example
    
    * fix: Handle clamscan failures greacefully
    
    * ci(lint): Linting fixes
    
    * fix: Show persona dates
    
    * ci(lint): Fix linting

--- ui/src/containers/Owner/CreatedAt.js
+++ ui/src/containers/Owner/CreatedAt.js
@@ -1,4 +1,6 @@
 import moment from 'moment';
 
-export default createdAt =>
-  `Created ${moment(createdAt).fromNow()} - ${moment(createdAt).format('YYYY-MM-DD HH:mm:ss')}`;
+export default (createdAt) => {
+  if (!createdAt) return '';
+  return `Created ${moment(createdAt).fromNow()} - ${moment(createdAt).format('YYYY-MM-DD HH:mm:ss')}`;
+};
