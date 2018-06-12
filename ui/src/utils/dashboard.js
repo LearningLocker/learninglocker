dc9fa2db9e56d6656e423ed96aaaba536c9ff602
Chris Bishop <chrisjbishop155@gmail.com>
2018-06-12 13:03:48 +0100

    Upgraded jsonwebtoken.

--- ui/src/utils/dashboard.js
+++ ui/src/utils/dashboard.js
@@ -1,4 +1,6 @@
 import getRouteUrl from 'ui/utils/getRouteUrl';
+import { JWT_SECURED } from 'lib/constants/dashboard';
+import jwt from 'jsonwebtoken';
 
 export const getShareableUrl = ({ // eslint-disable-line import/prefer-default-export
   model,
@@ -9,5 +11,10 @@ export const getShareableUrl = ({ // eslint-disable-line import/prefer-default-e
     .replace(/~-/g, '~');
   const urlEndocedTitle = encodeURI(title);
 
-  return `${getRouteUrl()}/dashboards/${parentModel.get('_id')}/${model.get('_id')}/${urlEndocedTitle}`;
+  if (model.get('filterMode') !== JWT_SECURED) {
+    return `${getRouteUrl()}/dashboards/${parentModel.get('_id')}/${model.get('_id')}/${urlEndocedTitle}`;
+  }
+
+  const jwtToken = jwt.sign({}, model.get('filterJwtSecret'));
+  return `${getRouteUrl()}/dashboards/${parentModel.get('_id')}/${model.get('_id')}/${urlEndocedTitle}?filter=${jwtToken}`;
 };
