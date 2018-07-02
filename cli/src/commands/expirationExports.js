6934bc7721529ce3fe9ac39877721f7d81ccb3d2
happy_machine <thisisdjfresh@gmail.com>
2018-07-02 09:28:57 +0100

    Started implementing storage delete.

--- cli/src/commands/expirationExports.js
+++ cli/src/commands/expirationExports.js
@@ -4,8 +4,9 @@ import { SITE_SETTINGS_ID } from 'lib/constants/siteSettings';
 import { get } from 'lodash';
 import moment from 'moment';
 import { map } from 'bluebird';
-import { publish as publishToQueue } from 'lib/services/queue';
 import Organisation from 'lib/models/organisation';
+// import { deleteFromPath } from 'lib/services/files/storage/local';
+import { factory } from 'lib/services/files/storage';
 
 
 const expireExportsForOrgatinasion = async (organisation) => {
@@ -15,21 +16,39 @@ const expireExportsForOrgatinasion = async (organisation) => {
   const todayDate = moment();
   const cutOffDate = todayDate.subtract(ttl, 'seconds');
 
-  await Download.deleteMany({
-    // organisation: organisation._id,
-    time: { $lt: cutOffDate }
-  }).exec();
-  console.log('003.1');
+  // await Download.deleteMany({
+  //   organisation: organisation._id,
+  //   time: { $lt: cutOffDate }
+  // }).exec();
+
+  const exports = await Download.find({
+    organisation: organisation._id,
+    upload: { $exists: true }
+    //   time: { $lt: cutOffDate } // DEBUG ONLY, uncomment
+  });
+
+
+  await map(exports, async (expor) => {
+    const repo = factory(expor.upload.repo);
+    try {
+      await repo.deleteFromPath(expor.upload.key);
+    } catch (err) {
+      console.error('err', err);
+    }
+
+    await Download.findByIdAndRemove(expor._id);
+  });
+
+  console.log('003.1', exports);
 };
 
 export default async function ({
   dontExit = false
 }) {
-
-  const siteSettings = await SiteSettings.find({
+  const siteSettings = await SiteSettings.findOne({
     _id: SITE_SETTINGS_ID
   });
-  if(get(siteSettings, 'expireExports', false) === false) {
+  if (get(siteSettings, 'expireExports', false) === false) {
     if (!dontExit) {
       process.exit();
     }
@@ -42,14 +61,9 @@ export default async function ({
 
   await map(organisations, expireExportsForOrgatinasion);
 
-  console.log('002', organisations);
-
 //   const toDeleteExport = await Download.find({
 //     time: { $gt: cutOffDate }
 //   });
-
-
-
 //   await map(toDeleteExport, async (organisation) => {
 //     if (
 //       todayDate.isAfter(moment(organisation.settings.EXPIRE_EXPORTS.ttl))
