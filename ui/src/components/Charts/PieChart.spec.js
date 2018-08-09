52e9b0207bdb25b625273436c81c4c9224d2c191
Chris Bishop <chrisjbishop155@gmail.com>
2018-08-09 13:51:36 +0100

    Added test for pie chart.

---
+++ ui/src/components/Charts/PieChart.spec.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { withInsertCSS } from 'ui/utils/hocs';
+import 'jest-enzyme';
+import { List, fromJS } from 'immutable';
+import { render, shallow } from 'enzyme';
+import { PieChartComponent } from './PieChart';
+
+
+test('PieChart should render no data', () => {
+  const pieChart = shallow(<PieChartComponent
+    results={new List()} />);
+
+  expect(pieChart).toMatchSnapshot();
+});
+
+test.only('PieChart should render', () => {
+  const results = fromJS([[{
+    0: {
+      _id: 0,
+      count: 1,
+      model: 0
+    },
+    1: {
+      _id: 1,
+      count: 5,
+      model: 1
+    }
+  }]]);
+
+  const pieChart = render(<PieChartComponent
+    results={results}
+    labels={fromJS([undefined])}
+    colors={fromJS(['#ffffff', '#123456'])} />);
+
+  expect(pieChart).toMatchSnapshot();
+});
