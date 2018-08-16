a313ca172a7a9f63643c703a85c470f0d05fd83a
James Mullaney <james@ht2.co.uk>
2018-08-16 15:27:01 +0000

    refactor(getPercentage): Remove boolean switch

--- ui/src/utils/defaultTitles.js
+++ ui/src/utils/defaultTitles.js
@@ -97,15 +97,18 @@ export const createVisualisationName = (model, prefix) => defaultSelector(model,
 export const createVisualisationText = (model, prefix = '') => defaultSelector(model, model.get('type', 'Unnamed'), prefix, TEXT);
 export const createDefaultTitleWithIcon = (model, name) => <span><VisualisationTypeIcon id={model.get('_id')} sourceView={model.get('sourceView')} /><span style={{ marginLeft: '3px' }}>{name || createVisualisationName(model)}</span></span>;
 export const createDefaultTitle = (model, prefix) => createVisualisationText(model, prefix);
+
 export const getPercentage = (res1, res2) => {
   const newValue = res1 || 0;
   const oldValue = res2 || 0;
   const percentage = parseInt(Math.round(((newValue - oldValue) / newValue) * 100));
-  let formattedResult;
-  switch (true) {
-    case res2 === 0 || isNaN(percentage): formattedResult = { result: 'N/A', color: '#9BA5AB' }; break;
-    case percentage < 0 : formattedResult = { result: `${percentage}%`, color: '#E73304' }; break;
-    default: formattedResult = { result: `+${percentage}%`, color: '#23A17E' }; break;
+  if (res2 === 0 || isNaN(percentage)) {
+    return { result: 'N/A', color: '#9BA5AB' };
   }
-  return formattedResult;
+
+  if (percentage < 0) {
+    return { result: `${percentage}%`, color: '#E73304' };
+  }
+
+  return { result: `+${percentage}%`, color: '#23A17E' };
 };
