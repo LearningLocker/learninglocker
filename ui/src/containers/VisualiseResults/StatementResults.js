9b3c8ff84675e31f5b4740b2a13375163f411446
Chris Bishop <chrisjbishop155@gmail.com>
2018-09-24 12:51:05 +0100

    New table

--- ui/src/containers/VisualiseResults/StatementResults.js
+++ ui/src/containers/VisualiseResults/StatementResults.js
@@ -1,16 +1,51 @@
 import React, { PropTypes } from 'react';
 import { withProps, compose, setPropTypes, defaultProps } from 'recompose';
 import { v4 as uuid } from 'uuid';
+import ReactTable from 'react-table';
 import { AutoSizer } from 'react-virtualized';
 import NoData from 'ui/components/Graphs/NoData';
 import { Map } from 'immutable';
 import { withStatementsVisualisation, withModels } from 'ui/utils/hocs';
-import styles from './styles.css';
 
 const columnWidth = {
   wordWrap: 'break-word',
   maxWidth: '150px'
 };
+
+const makeData = (results) => {
+  const makeResult = (row, header) => {
+    return {
+      header: header,
+      row: row,
+    }
+  }
+  const out = results
+    .first()
+    .first()
+    .map((res) => { 
+      return res
+        .get('statementColumns')
+        .keySeq()
+        .map((header) => {
+          if (res.get(header, '').toJS) {
+            return makeResults(JSON.stringify(res.get(header, '').toJS()), header);
+          }                  
+          return makeResults(res.get(header, ''), header);
+      });
+    });
+  return out;
+}
+
+const makeColumns = (data) => {
+  const out = data.map((row) => {
+    return {
+      Header: row.header,
+      accessor: 'row',
+      style: { textAlign: 'center' },
+      width: 100,
+    }
+  })
+}
 
 const enhance = compose(
   withStatementsVisualisation,
@@ -34,9 +69,28 @@ export default enhance(({
   model,
   results,
 }) => {
+  console.log('TCL:  model,results', model, results);
   if (results.size) {
     return (
-      <AutoSizer>{({ height, width }) => (    
+      <AutoSizer>{({ height, width }) => (
+        <div style={{ overflow: 'auto', height, width, position: 'relative' }}>
+          <ReactTable
+          data={makeData(results)}
+          pageSizeOptions={[15, 25, 50 ,100]}
+          defaultPageSize={15}
+          columns={makeColumns(results)}
+          className="-striped -highlight"
+          />
+        </div>
+      )
+      }</AutoSizer>
+    )
+  }
+  return (<NoData />);
+});
+
+    {/* return (
+      <AutoSizer>{({ height, width }) => (
         <div style={{ overflow: 'auto', height, width, position: 'relative' }}>
           <table className="table table-bordered table-striped">
             <tbody>
@@ -58,7 +112,4 @@ export default enhance(({
           </table>
         </div>)
       }</AutoSizer>
-    );
-  }
-  return (<NoData />);
-});
+    ); */}
