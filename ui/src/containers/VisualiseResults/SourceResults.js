304364f727d938b575f4595d3cd59b734bdb6da0
happy_machine <thisisdjfresh@gmail.com>
2018-10-18 22:08:19 +0100

    in progress

--- ui/src/containers/VisualiseResults/SourceResults.js
+++ ui/src/containers/VisualiseResults/SourceResults.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { compose } from 'recompose';
 import NoData from 'ui/components/Graphs/NoData';
-import { Map, OrderedMap } from 'immutable';
+import { Map, OrderedMap, toJS } from 'immutable';
 import isString from 'lodash/isString';
 import { withStatementsVisualisation } from 'ui/utils/hocs';
 import { getAxesString } from 'ui/utils/defaultTitles';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './styles.css';
+import { isUndefined } from 'lodash';
 import { displayVerb, displayActivity } from '../../utils/xapi';
 
 const moreThanOneSeries = tData => tData.first() !== undefined && tData.first().size > 1;
 
 export const generateTableData = (results, labels) => {
   const seriesList = labels.zip(results);
+  console.log('sl',seriesList)
   const seriesList2 = seriesList.map(([key, item], i) => {
+    console.log('yo',[key, item])
     if (key === undefined) {
       return [`Series ${i + 1}`, item];
     }
@@ -21,19 +24,44 @@ export const generateTableData = (results, labels) => {
   });
 
   const seriesMap = new OrderedMap(seriesList2);
-  const result = seriesMap.reduce((reduction, series, seriesKey) =>
-    series.reduce(
-      (axesReduction, axes2, axesKey) => axes2.reduce(
-        (seriesReduction, item) => {
-          const dataKeyName = [item.get('_id'), 'rowData', seriesKey, axesKey];
-          const modelKeyName = [item.get('_id'), 'model'];
-          return seriesReduction
-            .setIn(dataKeyName, item)
-            .setIn(modelKeyName, item.get('model'));
-        },
-      axesReduction),
+  const sizeCount = seriesMap.reduce((history, current) => {
+    console.log('hist,curr', history, current, current.first())
+    return Math.max(current.first().size, history)
+  }, 0)
+  console.log('seriesMap', seriesMap)
+  const result = seriesMap.reduce((reduction, series, seriesKey) => {
+    console.log('seriessss', series, series.size)
+    return series.reduce((axesReduction, axes2, axesKey) => {
+     
+      console.log('axes2',axes2, seriesMap)
+      const out = axes2.reduce((seriesReduction, item) => {
+        const dataKeyName = [item.get('_id'), 'rowData', seriesKey, axesKey];
+        const modelKeyName = [item.get('_id'), 'model'];
+        console.log('seriesr', seriesReduction);
+        return seriesReduction
+          .setIn(dataKeyName, item)
+          .setIn(modelKeyName, item.get('model'));
+      }, axesReduction)
+      console.log('size', out.length, out.size)
+      console.log('sizeCount', sizeCount)
+  
+      return out
+    },
     reduction)
+  }
   , new OrderedMap());
+  console.log('result', result)
+  const mindFuck = result.forEach((item,index) => {
+    console.log(item.get('rowData'))
+    labels.forEach((label,iterator) => {
+      console.log('it row',item.get('rowData')[iterator])
+      if (isUndefined(item.get('rowData')[iterator])){
+        console.log('got an undefined at ', iterator)
+        item.setIn('rowData', `Series ${iterator}`, 'Empty')
+      }
+    })
+
+  })
   return result;
 };
 
