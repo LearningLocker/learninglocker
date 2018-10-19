362a07087304b33651eff9fac4222f7a79c7bd02
happy_machine <thisisdjfresh@gmail.com>
2018-10-19 10:03:12 +0100

    problem with immutable merged keys

--- ui/src/containers/VisualiseResults/SourceResults.js
+++ ui/src/containers/VisualiseResults/SourceResults.js
@@ -1,67 +1,73 @@
 import React from 'react';
 import { compose } from 'recompose';
 import NoData from 'ui/components/Graphs/NoData';
-import { Map, OrderedMap, toJS } from 'immutable';
+import { Map, OrderedMap, fromJS } from 'immutable';
 import isString from 'lodash/isString';
 import { withStatementsVisualisation } from 'ui/utils/hocs';
 import { getAxesString } from 'ui/utils/defaultTitles';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './styles.css';
-import { isUndefined } from 'lodash';
 import { displayVerb, displayActivity } from '../../utils/xapi';
 
 const moreThanOneSeries = tData => tData.first() !== undefined && tData.first().size > 1;
 
 export const generateTableData = (results, labels) => {
   const seriesList = labels.zip(results);
-  console.log('sl',seriesList)
   const seriesList2 = seriesList.map(([key, item], i) => {
-    console.log('yo',[key, item])
     if (key === undefined) {
       return [`Series ${i + 1}`, item];
     }
     return [key, item];
   });
+  const setIdInSeries = (out, largestSeries) => {
+    const currentKeys = out.first().keySeq().toArray();
+    const missingKeys = largestSeries.keys.filter(key => !currentKeys.includes(key));
+    const objectToMerge = {};
+    missingKeys.forEach((key) => {
+      objectToMerge[key] = {
+        _id: key,
+        count: 0,
+        model: 'Empty'
+      };
+    });
+    return out.first().mergeDeep(fromJS(objectToMerge));
+  };
 
   const seriesMap = new OrderedMap(seriesList2);
-  const sizeCount = seriesMap.reduce((history, current) => {
-    console.log('hist,curr', history, current, current.first())
-    return Math.max(current.first().size, history)
-  }, 0)
-  console.log('seriesMap', seriesMap)
+  const largestSeries = seriesMap.reduce((history, current) => {
+    return {
+      size: Math.max(current.first().size, history.size),
+      keys: (() => {
+        if (current.first().size > history.size){
+          return current.first().keySeq().toArray();
+        }
+        return history.keys;
+      })()
+    };
+  }, { size: 0, series: 0 });
+
   const result = seriesMap.reduce((reduction, series, seriesKey) => {
-    console.log('seriessss', series, series.size)
-    return series.reduce((axesReduction, axes2, axesKey) => {
-     
-      console.log('axes2',axes2, seriesMap)
-      const out = axes2.reduce((seriesReduction, item) => {
-        const dataKeyName = [item.get('_id'), 'rowData', seriesKey, axesKey];
-        const modelKeyName = [item.get('_id'), 'model'];
-        console.log('seriesr', seriesReduction);
-        return seriesReduction
-          .setIn(dataKeyName, item)
-          .setIn(modelKeyName, item.get('model'));
-      }, axesReduction)
-      console.log('size', out.length, out.size)
-      console.log('sizeCount', sizeCount)
-  
-      return out
-    },
+    console.log('series', series)
+    if (series.size < largestSeries.size) {
+      series = setIdInSeries(series, largestSeries);
+    }
+    console.log('fixed series ', series)
+    const out = series.reduce(
+      (axesReduction, axes2, axesKey) => axes2.reduce(
+        (seriesReduction, item) => {
+          console.log('item!', item)
+          const dataKeyName = [item.get('_id') || item, 'rowData', seriesKey, axesKey];
+          const modelKeyName = [item.get('_id'), 'model'];
+          return seriesReduction
+            .setIn(dataKeyName, item)
+            .setIn(modelKeyName, item.get('model'));
+        },
+      axesReduction),
     reduction)
+    return out;
   }
   , new OrderedMap());
-  console.log('result', result)
-  const mindFuck = result.forEach((item,index) => {
-    console.log(item.get('rowData'))
-    labels.forEach((label,iterator) => {
-      console.log('it row',item.get('rowData')[iterator])
-      if (isUndefined(item.get('rowData')[iterator])){
-        console.log('got an undefined at ', iterator)
-        item.setIn('rowData', `Series ${iterator}`, 'Empty')
-      }
-    })
-
-  })
+  console.log('result size', result.size)
   return result;
 };
 
