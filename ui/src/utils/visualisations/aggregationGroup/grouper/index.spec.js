3191bd2470cc7ea7d75cfdc5c9ba6226a99884fd
h-kanazawa <kanazawahotaka@gmail.com>
2019-01-22 10:30:03 +0000

    test: Add tests for aggregationGroup grouper

--- ui/src/utils/visualisations/aggregationGroup/grouper/index.spec.js
+++ ui/src/utils/visualisations/aggregationGroup/grouper/index.spec.js
@@ -1,13 +1,35 @@
 import grouper from './index';
 
-test('aggrigationGroup grouper adds unwind pipeline stage', () => {
-  const result = grouper({
-    valueType: 'statements',
-    groupType: 'statement.context.contextActivities.category',
-    operatorType: 'uniqueCount'
+describe('aggregationGroup grouper', () => {
+  describe('when groupType is "date"', () => {
+    const result = grouper({
+      valueType: 'statements',
+      groupType: 'date',
+      operatorType: 'uniqueCount'
+    });
+
+    test('pipeline has correct stages', () => {
+      expect(result.toJS().map(stage => Object.keys(stage)[0]))
+        .toEqual([
+          '$project',
+          '$group',
+          '$sort',
+          '$limit',
+          '$project'
+        ]);
+    });
   });
 
-  expect(result.getIn([1, '$unwind']).toJS())
-    .toEqual({ path: '$statement.context.contextActivities.category' });
-});
+  describe('when groupType starts with "statement.context.contextActivities."', () => {
+    const result = grouper({
+      valueType: 'statements',
+      groupType: 'statement.context.contextActivities.category',
+      operatorType: 'uniqueCount'
+    });
 
+    test('pipeline has correct unwind stage', () => {
+      expect(result.getIn([1, '$unwind']).toJS())
+        .toEqual({ path: '$statement.context.contextActivities.category' });
+    });
+  });
+});
