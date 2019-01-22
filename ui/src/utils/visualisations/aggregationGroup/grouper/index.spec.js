32f682b596b84b491ef70b3ade96c31735f3fc49
h-kanazawa <kanazawahotaka@gmail.com>
2019-01-22 10:32:56 +0000

    fix: Fix grouping by Persona.Import in visualisations

--- ui/src/utils/visualisations/aggregationGroup/grouper/index.spec.js
+++ ui/src/utils/visualisations/aggregationGroup/grouper/index.spec.js
@@ -32,4 +32,48 @@ describe('aggregationGroup grouper', () => {
         .toEqual({ path: '$statement.context.contextActivities.category' });
     });
   });
+
+  describe('when groupType starts with "persona.import."', () => {
+    const result = grouper({
+      valueType: 'statements',
+      groupType: 'persona.import.abcde',
+      operatorType: 'uniqueCount'
+    });
+
+    test('pipeline has correct stages', () => {
+      expect(result.toJS().map(stage => Object.keys(stage)[0]))
+        .toEqual([
+          '$lookup',
+          '$unwind',
+          '$match',
+          '$project',
+          '$group',
+          '$sort',
+          '$limit',
+          '$project'
+        ]);
+    });
+
+    test('pipeline content is correct', () => {
+      expect(result.getIn([0, '$lookup']).toJS())
+      .toEqual({
+        from: 'personaAttributes',
+        as: 'personaAttrs',
+        localField: 'person._id',
+        foreignField: 'personaId'
+      });
+
+      expect(result.getIn([1, '$unwind']).toJS())
+        .toEqual({ path: '$personaAttrs' });
+
+      expect(result.getIn([2, '$match']).toJS())
+        .toEqual({ 'personaAttrs.key': 'abcde' });
+
+      expect(result.getIn([3, '$project']).toJS())
+        .toEqual({
+          group: '$personaAttrs.value',
+          model: '$personaAttrs.value',
+        });
+    });
+  });
 });
