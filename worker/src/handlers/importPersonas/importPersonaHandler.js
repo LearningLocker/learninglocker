5f488cb2ba3f7be84c7b9d9a399e433894fa1cd6
Ryan Smith <0ryansmith1994@gmail.com>
2019-02-27 16:29:19 +0000

    refactor: Makes requested changes.

--- worker/src/handlers/importPersonas/importPersonaHandler.js
+++ worker/src/handlers/importPersonas/importPersonaHandler.js
@@ -1,76 +1,10 @@
 import { publish } from 'lib/services/queue';
-import { promisify, map } from 'bluebird';
-import ImportPersonasLock from 'lib/models/importPersonasLock';
-import { getIfis } from 'lib/services/importPersonas/personasImportHelpers';
+import { promisify } from 'bluebird';
 import { PERSONA_IMPORT_QUEUE, STAGE_IMPORTED } from 'lib/constants/personasImport';
 import importPersona from 'lib/services/importPersonas/importPersona';
 import moment from 'moment';
 import PersonasImport from 'lib/models/personasImport';
 import { addErrorsToCsv } from 'lib/services/importPersonas/importPersonas';
-
-const LOCK_TIMEOUT = 120; // seconds
-
-// exported for testing
-export const establishLock = async ({
-  structure,
-  data,
-  organisation,
-  ifis = getIfis({
-    structure,
-    row: data
-  }), // private
-  lockTimeout = LOCK_TIMEOUT * 1000 // testing
-}) => {
-  try {
-    const result = await ImportPersonasLock.create({
-      organisation,
-      ifis
-    });
-
-    return result;
-  } catch (err) {
-    // DuplicateKey
-    if (err.code && err.code === 11000) {
-      const models = ImportPersonasLock.find({
-        organisation,
-        ifis: {
-          $in: ifis
-        }
-      });
-
-      let remainingLocks = false;
-      const deletePromises = map(models, (model) => {
-        if (moment(model.createdAt).add(lockTimeout, 'milliseconds').isBefore(moment())) {
-          return model.remove();
-        }
-        remainingLocks = true;
-      });
-
-      await deletePromises;
-      if (remainingLocks === false) {
-        return await establishLock({
-          structure,
-          data,
-          organisation,
-          ifis,
-          lockTimeout
-        });
-      }
-
-      return false;
-    }
-    throw err;
-  }
-};
-
-const releaseLock = async ({
-  lock
-}) => {
-  if (!lock) {
-    return;
-  }
-  await lock.remove();
-};
 
 export const finishedProcessing = async ({
   personaImportId
@@ -78,9 +12,9 @@ export const finishedProcessing = async ({
   await PersonasImport.updateOne({
     _id: personaImportId
   }, {
-    importStage: STAGE_IMPORTED,
-    importedAt: moment().toDate()
-  });
+      importStage: STAGE_IMPORTED,
+      importedAt: moment().toDate()
+    });
 
   const personasImport = await PersonasImport.findOne({
     _id: personaImportId
@@ -100,43 +34,29 @@ export default personaService => async ({
   structure,
   organisation
 }, done) => {
-  // establish lock
-  let lock;
-  const afterValidationCb = async () => {
-    lock = await establishLock({ structure, data, organisation });
-    if (!lock) {
-      await promisify(publish)({
-        queueName: PERSONA_IMPORT_QUEUE,
-        payload: {
-          index,
-          data,
-          personaImportId,
-          structure,
-          organisation
-        }
-      });
-      done();
-      return false;
+  try {
+    const { processedCount, totalCount } = await importPersona({
+      personaImportId,
+      structure,
+      organisation,
+      personaService
+    })(data, index);
+
+    if (totalCount && processedCount >= totalCount) {
+      await finishedProcessing({ personaImportId });
     }
-    return true;
-  };
-
-  // Do the magic
-  const { processedCount, totalCount } = await importPersona({
-    personaImportId,
-    structure,
-    organisation,
-    personaService,
-    afterValidationCb
-  })(data, index);
-
-  // have we finished processing ???
-  if (totalCount && processedCount >= totalCount) {
-    await finishedProcessing({ personaImportId });
+  } catch (err) {
+    await promisify(publish)({
+      queueName: PERSONA_IMPORT_QUEUE,
+      payload: {
+        index,
+        data,
+        personaImportId,
+        structure,
+        organisation
+      }
+    });
   }
-
-  // release lock
-  await releaseLock({ lock });
 
   done();
 };
