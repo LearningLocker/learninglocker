f58d58501ba249ba71bcd7de63a0ad7db231f904
h-kanazawa <h-kanazawa@users.noreply.github.com>
2019-03-13 12:35:13 +0000

    feat(Visualisations Filter): Add contextActivities.definition.type filter (#1314)

--- ui/src/utils/visualisations/aggregateChart.js
+++ ui/src/utils/visualisations/aggregateChart.js
@@ -5,7 +5,8 @@ export default (preReqs, axes) => {
   const valueType = axes.getIn(['value', 'optionKey'], 'statements');
   const groupType = axes.getIn(['group', 'optionKey'], 'date');
   const operatorType = axes.get('operator', 'uniqueCount');
-  const aggregationGroup = grouper({ valueType, groupType, operatorType });
+  const contextActivityDefinitionType = axes.getIn(['group', 'contextActivityDefinitionType'], null);
+  const aggregationGroup = grouper({ valueType, groupType, operatorType, contextActivityDefinitionType });
   const pipeline = preReqs.concat(fromJS(aggregationGroup));
   return new List([pipeline]);
 };
