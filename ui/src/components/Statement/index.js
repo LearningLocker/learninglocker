4832dde69fd1cc50a1250423c7420ed1760c6647
h-kanazawa <kanazawahotaka@gmail.com>
2019-03-27 15:52:43 +0000

    refactor: Statement component

--- ui/src/components/Statement/index.js
+++ ui/src/components/Statement/index.js
@@ -18,23 +18,20 @@ class Statement extends Component {
   hasInStatement = path =>
     this.props.statement.hasIn(path);
 
-  setFilterAt = (keyPath, value, event) => {
-    event.preventDefault();
-    event.stopPropagation();
-    this.props.setFilterAt(keyPath, value);
-  }
-
   renderPart = (path, part, displayer, filterPath = path) => {
     const value = this.getInStatement(path);
     const filterValue = path === filterPath ? value : this.getInStatement(filterPath);
     const display = displayer(value);
     const title = JSON.stringify(value.toJS(), null, 2);
-    const setFilter = this.setFilterAt.bind(null, filterPath, filterValue);
 
     return (
       <a
         className={styles[part]}
-        onClick={setFilter}
+        onClick={(event) => {
+          event.preventDefault();
+          event.stopPropagation();
+          this.props.setFilterAt(filterPath, filterValue);
+        }}
         title={title}>
         {display}
       </a>
@@ -50,55 +47,50 @@ class Statement extends Component {
   renderActivity = path =>
     this.renderPart(path, 'object', displayActivity);
 
-  renderStatement = basePath => (
-    <span>
-      {this.renderActor(basePath.concat(['actor']), (
-        basePath.size === 1 && this.hasInStatement(new List(['person'])) ?
-        new List(['person']) :
-        basePath.concat(['actor'])
-      ))}{' '}
-      {this.renderVerb(basePath.concat(['verb']))}{' '}
-      {this.renderObject(basePath.concat(['object']))}
-    </span>
-  );
-
   renderSubStatement = basePath =>
     <span>({this.renderStatement(basePath)})</span>;
-
-  renderStatementRef = () =>
-    <span>StatementRef</span>;
 
   renderObject = (basePath) => {
     const objectTypePath = basePath.concat(['objectType']);
     const objectType = this.getInStatement(objectTypePath);
+
     switch (objectType) {
       case 'SubStatement': return this.renderSubStatement(basePath);
-      case 'StatementRef': return this.renderStatementRef(basePath);
+      case 'StatementRef': return (<span>StatementRef</span>);
       case 'Agent':
       case 'Group': return this.renderActor(basePath);
       default: return this.renderActivity(basePath);
     }
   }
 
-  renderTimestamp = (basePath) => {
-    const timestampPath = basePath.concat(['timestamp']);
-    const timestamp = moment(this.getInStatement(timestampPath));
-    return (
-      <AutoUpdate>
-        <span className={`pull-right ${styles.timestamp}`}>
-          {timestamp.fromNow()}
-        </span>
-      </AutoUpdate>
-    );
-  }
-
-  render = () => {
-    const basePath = new List(['statement']);
+  renderStatement = basePath => {
+    const actorPath = basePath.size === 1 && this.hasInStatement(new List(['person'])) ?
+      new List(['person']) :
+      basePath.concat(['actor']);
 
     return (
       <span>
-        {this.renderTimestamp(basePath)}
-        {this.renderStatement(basePath)}
+        {this.renderActor(basePath.concat(['actor']), actorPath)}
+        {' '}
+        {this.renderVerb(basePath.concat(['verb']))}
+        {' '}
+        {this.renderObject(basePath.concat(['object']))}
+      </span>
+    );
+  };
+
+  render = () => {
+    const timestamp = moment(this.getInStatement(new List(['statement', 'timestamp'])));
+
+    return (
+      <span>
+        {this.renderStatement(new List(['statement']))}
+
+        <AutoUpdate>
+          <span className={`pull-right ${styles.timestamp}`}>
+            {timestamp.fromNow()}
+          </span>
+        </AutoUpdate>
       </span>
     );
   };
