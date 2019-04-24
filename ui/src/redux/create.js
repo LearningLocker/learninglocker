e188a462b4ff57822a7a176663cd087bb6a03ee5
h-kanazawa <kanazawahotaka@gmail.com>
2019-04-24 10:41:35 +0100

    Migrate devToolsExtension to __REDUX_DEVTOOLS_EXTENSION_COMPOSE__

--- ui/src/redux/create.js
+++ ui/src/redux/create.js
@@ -16,14 +16,15 @@ export default function createStore(llClient, router, data) {
     router5Middleware(router),
   ];
 
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
   let finalCreateStore;
   if (__DEVELOPMENT__ && __CLIENT__ && __DEVTOOLS__) {
     const { persistState } = require('redux-devtools');
     const DevTools = require('../containers/DevTools/DevTools');
 
-    finalCreateStore = compose(
+    finalCreateStore = composeEnhancers(
       applyMiddleware(...middleware),
-      window.devToolsExtension ? window.devToolsExtension() : DevTools.instrument(),
       persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
     )(_createStore);
   } else {
