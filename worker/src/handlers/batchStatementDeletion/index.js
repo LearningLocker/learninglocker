4a06b807c76af038c9c47afa76007b8304ab1323
James Mullaney <james@ht2.co.uk>
2019-04-26 13:28:46 +0100

    Add VisibilityTimeout to batch queue

--- worker/src/handlers/batchStatementDeletion/index.js
+++ worker/src/handlers/batchStatementDeletion/index.js
@@ -11,6 +11,7 @@ const defaultHandleResponse = (err) => {
 export default () => {
   Queue.subscribe({
     queueName: BATCH_STATEMENT_DELETION_QUEUE,
-    handler: batchStatementDeletion
+    handler: batchStatementDeletion,
+    visibilityTimeout: 60 * 60 // 1 hour
   }, defaultHandleResponse);
 };
