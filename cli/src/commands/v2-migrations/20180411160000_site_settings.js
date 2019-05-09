01a73012ddf952a13560bb47101abb42b2778a3a
James Mullaney <james@ht2.co.uk>
2019-05-09 15:14:40 +0100

    add logger

--- cli/src/commands/v2-migrations/20180411160000_site_settings.js
+++ cli/src/commands/v2-migrations/20180411160000_site_settings.js
@@ -1,6 +1,7 @@
 import SiteSettings from 'lib/models/siteSettings';
 import { SITE_SETTINGS_ID } from 'lib/constants/siteSettings';
 import { getConnection } from 'lib/connections/mongoose';
+import logger from 'lib/logger';
 import mongoose from 'mongoose';
 
 const objectId = mongoose.Types.ObjectId;
