532becaa7fa663aeed21e88f15835f6e501200f4
asahd <asahd@users.noreply.github.com>
2019-05-14 14:20:23 +0100

    Update 20190509000000_migrate_null_resetTokens.js

--- cli/src/commands/v2-migrations/20190509000000_migrate_null_resetTokens.js
+++ cli/src/commands/v2-migrations/20190509000000_migrate_null_resetTokens.js
@@ -4,7 +4,7 @@ import { getConnection } from 'lib/connections/mongoose';
 
 const up = async () => {
   const connection = getConnection();
-  logger.info('Updating all resetTokens with no expiry to have expiry from 24 hours from now.')
+  logger.info('Updating all resetTokens with null expiry to expire in 24 hours from now.')
   const newExpiry = moment().add(1, 'day').toDate();
   await connection.collection('users').update({"resetTokens.expires": null}, {$set: {"resetTokens.$.expires": newExpiry }});
 };
