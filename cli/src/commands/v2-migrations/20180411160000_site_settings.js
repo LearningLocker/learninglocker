850489c26c0b9505f6e138e71ede92b0564f3398
cbishopvelti <chrisjbishop155@gmail.com>
2019-05-14 14:21:40 +0100

    fix(resetTokens): Improved resetToken expiry logic (#1358)
    * Issue 938
    
    * Migration to apply expiry to all existing resetTokens with expiry of null
    
    * Supress siteSettings error on new user
    
    * amended copy for emails

--- cli/src/commands/v2-migrations/20180411160000_site_settings.js
+++ cli/src/commands/v2-migrations/20180411160000_site_settings.js
@@ -1,14 +1,20 @@
 import SiteSettings from 'lib/models/siteSettings';
 import { SITE_SETTINGS_ID } from 'lib/constants/siteSettings';
 import { getConnection } from 'lib/connections/mongoose';
+import logger from 'lib/logger';
 import mongoose from 'mongoose';
 
 const objectId = mongoose.Types.ObjectId;
 
 const up = async () => {
-  await new SiteSettings({
-    _id: objectId(SITE_SETTINGS_ID)
-  }).save();
+  try {
+    await new SiteSettings({
+      _id: objectId(SITE_SETTINGS_ID)
+    }).save();
+  } catch (err) {
+    if (err.code === 11000) return;
+    logger.error(err);
+  }
 };
 
 const down = async () => {
--- cli/src/commands/v2-migrations/index.js
+++ cli/src/commands/v2-migrations/index.js
@@ -4,23 +4,22 @@ import updateRefs from './20171121153300_update_refs';
 import expiration from './20180212_expiration';
 import migrateIdentifiers from './20171127214900_migrate_identifiers';
 import personasIndexes from './20171008104700_personas_indexes';
-// import removeOldIdents from './20171128144900_remove_old_idents';
 import siteSettings from './20180411160000_site_settings';
 import shareableDashboards from './20180320093000_shareable_dashboards';
 import personaAttributesIndexes from './20190212150000_personaAttributes_indexes';
 import oauthTokens from './20190131000000_oauth_tokens';
 import migrateToInQueries from './20190307130000_migrate_to_in_queries';
+import migrateResetTokens from './20190509000000_migrate_null_resetTokens';
 
 export default new OrderedMap()
   .set('20171122100800_common_indexes', commonIndexesMigration)
   .set('20171121153300_update_refs', updateRefs)
   .set('20171008104700_personas_indexes', personasIndexes)
   .set('20171127214900_migrate_identifiers', migrateIdentifiers)
-  // .set('20171127214500_remove_unused_persona_props', removeUnusedPersonaProps)
-  // .set('20171128144900_remove_old_idents', removeOldIdents)
   .set('20180320093000_shareable_dashboards', shareableDashboards)
   .set('20180411160000_site_settings', siteSettings)
   .set('20180212_expiration', expiration)
   .set('20190131000000_oauth_tokens', oauthTokens)
   .set('20190212150000_personaAttributes_indexes', personaAttributesIndexes)
-  .set('20190307130000_migrate_to_in_queries', migrateToInQueries);
+  .set('20190307130000_migrate_to_in_queries', migrateToInQueries)
+  .set('20190509000000_migrate_null_resetTokens', migrateResetTokens);
