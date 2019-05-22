cb653ace0435d47a8c46d25af96048483864c8dd
h-kanazawa <kanazawahotaka@gmail.com>
2019-05-22 09:24:07 +0100

    fix(JsonTextArea)

--- ui/src/components/JsonTextArea/index.js
+++ ui/src/components/JsonTextArea/index.js
@@ -35,18 +35,18 @@ export default class JsonTextArea extends Component {
     this.state = stateFromProps(props);
   }
 
-  componentDidUpdate = () => {
+  componentWillReceiveProps = (nextProps) => {
     const oldValue = this.state.value;
-    const stateWithValue = stateFromProps(this.props);
+    const stateWithValue = stateFromProps(nextProps);
     const selectionStart = cursorPosition({
-      oldCursorPosition: this.textarea._rootDOMNode.selectionStart,
+      oldCursorPosition: this.textarea._ref.selectionStart,
       oldValue,
       newValue: stateWithValue.value,
     });
 
     const selectionDifference =
-      this.textarea._rootDOMNode.selectionEnd -
-        this.textarea._rootDOMNode.selectionStart;
+      this.textarea._ref.selectionEnd -
+        this.textarea._ref.selectionStart;
 
     const stateWithCursor = {
       selectionStart,
@@ -67,8 +67,8 @@ export default class JsonTextArea extends Component {
       selectionEnd
     } = this.state;
 
-    this.textarea._rootDOMNode.selectionStart = selectionStart;
-    this.textarea._rootDOMNode.selectionEnd = selectionEnd;
+    this.textarea._ref.selectionStart = selectionStart;
+    this.textarea._ref.selectionEnd = selectionEnd;
   }
 
   textarea = null
