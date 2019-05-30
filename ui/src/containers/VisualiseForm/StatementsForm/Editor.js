2dcbb15121349e84a323f9c7d8ca735f633865e0
h-kanazawa <kanazawahotaka@gmail.com>
2019-05-30 18:44:52 +0100

    refactor(StatementsForm): refactor Editor

--- ui/src/containers/VisualiseForm/StatementsForm/Editor.js
+++ ui/src/containers/VisualiseForm/StatementsForm/Editor.js
@@ -1,17 +1,14 @@
 import React, { Component, PropTypes } from 'react';
 import { Map } from 'immutable';
 import { connect } from 'react-redux';
-import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import { updateModel } from 'ui/redux/modules/models';
-import Tabs from 'ui/components/Material/Tabs';
 import { Tab } from 'react-toolbox/lib/tabs';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import Tabs from 'ui/components/Material/Tabs';
+import { updateModel } from 'ui/redux/modules/models';
+import styles from '../visualiseform.css';
 import SeriesEditor from './SeriesEditor';
 import AxesEditor from './AxesEditor/AxesEditor';
-import styles from '../visualiseform.css';
 import OptionsEditor from './OptionsEditor';
-
-
-const SCHEMA = 'visualisation';
 
 class Editor extends Component {
   static propTypes = {
@@ -20,55 +17,35 @@ class Editor extends Component {
   }
 
   state = {
-    step: 0,
+    tabIndex: 0,
   }
+
+  onChangeTab = tabIndex => this.setState({ tabIndex })
 
   onChangeDescription = (e) => {
     this.props.updateModel({
-      schema: SCHEMA,
+      schema: 'visualisation',
       id: this.props.model.get('_id'),
       path: 'description',
       value: e.target.value
     });
   }
 
-  changeStep = step =>
-    this.setState({ step })
-
-  // This method is for LearningLocker/enterprise
+  // This method looks strange, but it is used on LearningLocker/enterprise
   isSeriesType = () => false;
 
-  renderDescription = description => (
-    <div className="form-group">
-      <label htmlFor="lrsDescriptionInput">Name</label>
-      <input
-        id="lrsDescriptionInput"
-        className="form-control"
-        placeholder="What does this visualisation show?"
-        value={description}
-        onChange={this.onChangeDescription} />
-    </div>
-  )
-
-  renderTabs = () => {
-    // The Tabs component requires its children to be Tab items
-    // We cannot do inline conditionals, therefore we construct the children and pass them in via the props
-    const tabs = [
-      <Tab key="axes" label="Axes">
-        <AxesEditor
-          model={this.props.model} />
-      </Tab>,
-      <Tab key="options" label="Options">{ this.renderOptionsEditor() }</Tab>
-    ];
+  render = () => {
+    // This if-conditional is used on LearningLocker/enterprise
+    if (this.isSeriesType()) {
+      return (
+        <SeriesEditor
+          model={this.props.model}
+          exportVisualisation={this.props.exportVisualisation} />
+      );
+    }
 
     const isCounter = (this.props.model.get('type') === 'COUNTER');
-    const seriesTab = <Tab key="series" label="Series">{ this.renderSeriesEditor() }</Tab>;
-    const StatementSeriesTab = <Tab key="filter" label="Filter">{ this.renderSeriesEditor() }</Tab>;
-    if (isCounter) {
-      tabs.splice(1, 0, StatementSeriesTab);
-    } else {
-      tabs.splice(1, 0, seriesTab);
-    }
+
     return (
       <div className={styles.tab}>
         <div className="form-group">
@@ -81,27 +58,23 @@ class Editor extends Component {
             onChange={this.onChangeDescription} />
         </div>
 
-        <Tabs index={this.state.step} onChange={this.changeStep}> children={tabs}</Tabs>
+        <Tabs index={this.state.tabIndex} onChange={this.onChangeTab}>
+          <Tab key="axes" label="Axes">
+            <AxesEditor model={this.props.model} />
+          </Tab>
+
+          <Tab key="series" label={isCounter ? 'Filter' : 'Series'}>
+            <SeriesEditor
+              model={this.props.model}
+              exportVisualisation={this.props.exportVisualisation} />
+          </Tab>
+
+          <Tab key="options" label="Options">
+            <OptionsEditor model={this.props.model} />
+          </Tab>
+        </Tabs>
       </div>
     );
-  }
-
-  renderSeriesEditor = () => (
-    <SeriesEditor
-      model={this.props.model}
-      exportVisualisation={this.props.exportVisualisation} />
-  )
-
-  renderOptionsEditor = () => (
-    <OptionsEditor
-      model={this.props.model} />
-  )
-
-  render = () => {
-    if (this.isSeriesType()) {
-      return this.renderSeriesEditor();
-    }
-    return this.renderTabs();
   }
 }
 
