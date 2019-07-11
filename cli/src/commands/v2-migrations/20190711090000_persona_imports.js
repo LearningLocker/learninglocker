00fda7f6968930b3ec20bf188eb5f70521e354f0
Chris Bishop <chris.bishop@ht2labs.com>
2019-07-11 12:46:46 +0100

    Added down.

--- cli/src/commands/v2-migrations/20190711090000_persona_imports.js
+++ cli/src/commands/v2-migrations/20190711090000_persona_imports.js
@@ -43,7 +43,39 @@ const up = async () => {
 };
 
 const down = async () => {
-  logger.info('No down action persona imports migration');
+  const personasImportsSream = highland(PersonasImports.find({}).cursor());
+
+  const migrationStream = personasImportsSream.batch(BATCH_SIZE).flatMap((personasImports) => {
+    const donePromise = bmap(personasImports, async (personasImport) => {
+      const toMove = filter(toPairs(personasImport.structure), (structurePair) => {
+        const structure = structurePair[1];
+        return get(structure, 'columnType') === 'COLUMN_ACCOUNT_VALUE';
+      });
+      if (toMove.length === 0) {
+        // Nothing to do
+        return;
+      }
+
+      const newStructure = cloneDeep(personasImport.structure);
+
+      forEach(toMove, (toMov) => {
+        const key = toMov[0];
+        const value = toMov[1];
+        set(newStructure, [get(value, 'relatedColumn'), 'primary'], get(value, 'primary'));
+        unset(newStructure, [key, 'primary']);
+      });
+
+      personasImport.structure = newStructure;
+      await personasImport.save();
+    }, { concurrency: 1 });
+
+    return highland(donePromise);
+  });
+
+  await new Promise((resolve, reject) => {
+    migrationStream.on('error', reject);
+    migrationStream.apply(resolve);
+  });
 };
 
 export default { up, down };
