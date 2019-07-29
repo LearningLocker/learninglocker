831fcd358785575212f36364942f679f88159a23
Lydia Ross <lydiamross@live.com>
2019-07-29 10:15:47 +0100

    fix: Missing brackets on organisationId

--- ui/src/containers/Apps/index.js
+++ ui/src/containers/Apps/index.js
@@ -38,101 +38,85 @@ const Apps = ({ organisationId }) => (
         description="Use statements to trigger follow on actions, deliver personalized feedback and automate workflow."
         icon={icons.sparks}
         icon2x={icons.sparks2x} />
-
       <HrefAppCard
         hrefLink="https://ht2ltd.zendesk.com/hc/en-us/articles/208166165-xAPI-in-Curatr"
         title="Curatr LXP"
         description="Cutting edge Learning experience platform that is designed to work hand-in-hand with Learning Locker."
         icon={icons.curatr}
         icon2x={icons.curatr2x} />
-
       <HrefAppCard
         hrefLink="https://github.com/xAPI-vle/moodle-logstore_xapi"
         title="Moodle"
         description="Widely supported open source learning platform supported by a custom xAPI plugin."
         icon={icons.moodle}
         icon2x={icons.moodle2x} />
-
       <HrefAppCard
         hrefLink="https://www.ht2labs.com/apps/semantic-analysis/"
         title="Semantic Analysis"
         description="Use our proprietary algorithm to drive insights into the quality of learners' submissions."
         icon={icons.semanticAnalysis} />
-
       <HrefAppCard
         hrefLink="https://www.ht2labs.com/apps/bi-connector/"
         title="BI Connector"
         description="Feed your BI tool with real-time data from Learning Locker®."
         icon={icons.biConnector} />
-
       <HrefAppCard
         hrefLink="https://www.ht2labs.com/apps/survey-monkey/"
         title="Survey Monkey"
         description="Capture learner feedback from outside of the LMS"
         icon={icons.surveyMonkey} />
-
       <HrefAppCard
         hrefLink="https://www.ht2labs.com/apps/getabstract/"
         title="getAbstract"
         description="Summarize text from thousands of sources"
         icon={icons.getAbstract} />
-
       <HrefAppCard
         hrefLink="https://www.ht2labs.com/apps/degreed/"
         title="Degreed"
         description="API integration with one of the largest off the shelf learning platforms"
         icon={icons.degreed} />
-
       <HrefAppCard
         hrefLink="https://www.ht2labs.com/apps/cornerstone-on-demand/"
         title="Cornerstone On Demand"
         description="One of the most widely used LMSs on the market."
         icon={icons.cornerstoneOnDemand} />
-
       <HrefAppCard
         hrefLink="https://www.ht2labs.com/apps/google-forms/"
         title="Google Forms"
         description="Generate xAPI data from your Google forms responses"
         icon={icons.googleForms} />
-
       <HrefAppCard
         hrefLink="https://www.ht2labs.com/apps/yammer/"
         title="Yammer"
         description="Convert conversation data into xAPI statements"
         icon={icons.yammer} />
-
       <HrefAppCard
         hrefLink="https://www.ht2labs.com/apps/skillsoft/"
         title="SkillSoft"
         description="One of the most widely used off-the-shelf content providers in the world"
         icon={icons.skillsoft} />
-
       <HrefAppCard
         hrefLink="https://www.ht2labs.com/apps/url-shortener/"
         title="URL Shortener"
         description="Create simple shareable URLs"
         icon={icons.urlShortener} />
-
       <HrefAppCard
         hrefLink="https://www.ht2labs.com/apps/csv-to-xapi/"
         title="CSV to xAPI"
         description="Convert spreadsheet data into xAPI statements"
         icon={icons.csvToXAPI} />
-
       <HrefAppCard
         hrefLink="https://www.ht2labs.com/apps/launchr/"
         title="Launchr"
         description="Securely launch xAPI content packages"
         icon={icons.launchr} />
-
       <HrefAppCard
         hrefLink="https://www.ht2labs.com/apps/gdpr/"
         title="GDPR"
         description="A central administration tool for organizations seeking to comply with GDPR"
         icon={icons.gdpr} />
-
       <LinkAppCard
-        organisationId={organisationId}
+        organisationId={{ organisationId }}
         routeName="organisation.demo"
         title="Sales Demo"
         description="Provides example data to demo how Learning Locker works"
