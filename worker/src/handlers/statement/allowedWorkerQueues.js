82cdb7519521bd296a4b8e0ac9a41e1e7998a19e
h-kanazawa <kanazawahotaka@gmail.com>
2019-08-02 10:35:30 +0100

    Modify getAllowedWorkerQueues

--- worker/src/handlers/statement/allowedWorkerQueues.js
+++ worker/src/handlers/statement/allowedWorkerQueues.js
@@ -8,17 +8,17 @@ import {
 /**
  * allowable Worker Queues
  *
- * @type {Map<string, true>}
+ * @type {string[]}
  */
-const allowableWorkerQueues = new Map([
-  [STATEMENT_QUERYBUILDERCACHE_QUEUE, true],
-  [STATEMENT_EXTRACT_PERSONAS_QUEUE, true],
-  [STATEMENT_FORWARDING_QUEUE, true],
-]);
+const allowableWorkerQueues = [
+  STATEMENT_QUERYBUILDERCACHE_QUEUE,
+  STATEMENT_EXTRACT_PERSONAS_QUEUE,
+  STATEMENT_FORWARDING_QUEUE,
+];
 
 /**
  * @param {string | undefined} allowedWorkerQueuesString
- * @return {Map<string, true>}
+ * @return {string[]}
  * @throws {Error}
  */
 const getAllowedWorkerQueues = (allowedWorkerQueuesString) => {
@@ -27,25 +27,25 @@ const getAllowedWorkerQueues = (allowedWorkerQueuesString) => {
   }
 
   if (allowedWorkerQueuesString === '') {
-    return new Map();
+    return [];
   }
 
   return allowedWorkerQueuesString.split(',').reduce(
-    (accMap, queueString) => {
-      if (allowableWorkerQueues.has(queueString)) {
-        return accMap.set(queueString, queueString);
+    (acc, queueString) => {
+      if (allowableWorkerQueues.includes(queueString)) {
+        return acc.concat([queueString]);
       }
-      logger.warn(`"${queueString}" is ignored as an allowed worker queue. Allowable worker queues are "${STATEMENT_QUERYBUILDERCACHE_QUEUE}", "${STATEMENT_EXTRACT_PERSONAS_QUEUE}", and "${STATEMENT_FORWARDING_QUEUE}"`);
-      return accMap;
+      logger.warn(`"${queueString}" is ignored as an allowed worker queue. Allowable worker queues are ${allowableWorkerQueues.map(q => `"${q}"`).join(', ')}`);
+      return acc;
     },
-    new Map(),
+    [],
   );
 };
 
 /**
- * @type {Map<string, true>}
+ * @type {string[]}
  */
 const allowedWorkerQueues = getAllowedWorkerQueues(process.env.ALLOWED_WORKER_QUEUES);
 
 /* eslint-disable import/prefer-default-export*/
-export const isAllowedWorkerQueue = (queueName) => allowedWorkerQueues.has(queueName);
+export const isAllowedWorkerQueue = (queueName) => allowedWorkerQueues.includes(queueName);
