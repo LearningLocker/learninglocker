e957e3cc3ea4e278033d778a222d39c038906748
h-kanazawa <h-kanazawa@users.noreply.github.com>
2019-08-20 09:10:11 +0100

    feat: Adds copy button to dashboards and visualisations ([#1414](https://github.com/LearningLocker/learninglocker/pull/1414) - [LL-26](https://learningpool.atlassian.net/browse/LL-26))

--- ui/src/redux/modules/dashboard/index.js
+++ ui/src/redux/modules/dashboard/index.js
@@ -1,4 +1,5 @@
 import * as blankDashboard from './blankDashboard';
+import * as copyDashboard from './copyDashboard';
 import * as gettingStarted from './gettingStarted';
 
 /*
@@ -7,5 +8,6 @@ import * as gettingStarted from './gettingStarted';
 /* eslint-disable import/prefer-default-export */
 export const sagas = [
   ...blankDashboard.sagas,
+  ...copyDashboard.sagas,
   ...gettingStarted.sagas,
 ];
