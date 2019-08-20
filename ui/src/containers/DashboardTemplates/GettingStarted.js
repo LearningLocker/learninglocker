d78fcce4612364c83192a0c69229d881b40b0419
h-kanazawa <h-kanazawa@users.noreply.github.com>
2019-08-20 12:55:08 +0100

    feat: Improves dashboard template icons (https://github.com/LearningLocker/learninglocker/pull/1424 - [LL-135](https://learningpool.atlassian.net/browse/LL-135))

--- ui/src/containers/DashboardTemplates/GettingStarted.js
+++ ui/src/containers/DashboardTemplates/GettingStarted.js
@@ -3,11 +3,13 @@ import { withProps, compose, withHandlers } from 'recompose';
 import { loggedInUserId } from 'ui/redux/modules/auth';
 import { activeOrgIdSelector } from 'ui/redux/modules/router';
 import { CREATE_GETTING_STARTED } from 'ui/redux/modules/dashboard/gettingStarted';
+import iconImage from './assets/getting-started.png';
 import TemplateCard from './TemplateCard';
 
 const enhance = compose(
   withProps({
     title: 'Getting Started',
+    image: iconImage,
   }),
   connect(
     state => ({
