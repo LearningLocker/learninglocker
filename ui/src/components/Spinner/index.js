dd0aa1c14dadde06777abc209776d9491eefe38e
Vadim Nikolaenko <vadimnikolaenko.biz@gmail.com>
2020-01-16 14:27:31 +0200

    refactor(Spinner): Removes CSS from spinner

--- ui/src/components/Spinner/index.js
+++ ui/src/components/Spinner/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import styles from './styles.css';
+import styled from 'styled-components';
 
-const spinner = () => (
-  <div className={styles.spinner} />
-);
 
-export default withStyles(styles)(spinner);
+const Spinner = styled.div`
+  height: 30px;
+  width: 30px;
+  margin: auto;
+  animation: rotation .6s infinite linear;
+  border-left: 4px solid rgba(245, 170, 53, 0.15);
+  border-right: 4px solid rgba(245, 170, 53, 0.15);
+  border-bottom: 4px solid rgba(245, 170, 53, 0.15);
+  border-top: 4px solid rgba(245, 170, 53, 0.8);
+  border-radius: 100%;
+`;
+
+const spinner = () => (<Spinner />);
+
+export default spinner;
