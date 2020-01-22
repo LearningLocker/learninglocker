cfdb692784147356f4a83d222d2bd9b3074d0d6e
Vadim Nikolaenko <vadimnikolaenko.biz@gmail.com>
2020-01-22 11:34:23 +0200

    refactor(MaterialSwitch): Removes CSS from material switch

--- ui/src/components/Material/Switch/index.js
+++ ui/src/components/Material/Switch/index.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import Switch from 'react-toolbox/lib/switch';
-import { omitBy, isFunction } from 'lodash';
-import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import styles from './styles.css';
+import styled from 'styled-components';
 
-export default withStyles(styles)(props =>
-  <Switch
-    theme={omitBy(styles, isFunction)}
+const rippleClassName = 'ms-ripple';
+const offClassName = 'ms-off';
+const onClassName = 'ms-on';
+const thumbClassName = 'ms-thumb';
+
+const StyledSwitch = styled(Switch)`
+  .${rippleClassName} {
+    background-color: rgb(245,171,53);
+    border-color: rgb(245,171,53);
+  }
+
+  .${offClassName} {
+    background-color: rgba(0, 0, 0, 0.26);
+
+    .${thumbClassName} {
+      background-color: white;
+    }
+  }
+
+  .${onClassName} {
+    background-color: rgba(245,171,53, 0.26);
+
+    .${thumbClassName} {
+      background-color: rgb(245,171,53);
+    }
+  }
+`;
+
+export default props => (
+  <StyledSwitch
+    theme={{
+      ripple: rippleClassName,
+      off: offClassName,
+      on: onClassName,
+      thumb: thumbClassName,
+    }}
     {...props} />
 );
