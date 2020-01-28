5ee5d50b11102be72d35c6bc85c4c80a3b0ef552
Vadim Nikolaenko <vadimnikolaenko.biz@gmail.com>
2020-01-28 16:30:09 +0200

    test(BooleanCriterion): Fix tests

--- ui/src/containers/BasicQueryBuilder/Boolean/Criterion.spec.js
+++ ui/src/containers/BasicQueryBuilder/Boolean/Criterion.spec.js
@@ -1,58 +1,55 @@
 import React from 'react';
-import ReactTestRenderer from 'react-test-renderer';
-import { withInsertCSS } from 'ui/utils/hocs';
+import 'jest-styled-components';
+import renderer from 'react-test-renderer';
 import { Map } from 'immutable';
 import { initialSections } from 'ui/redux/modules/queryBuilder';
-import CriterionWithStyles, { Criterion } from './Criterion.js';
+import Criterion from './Criterion.js';
 
-const WrappedCriterion = withInsertCSS(CriterionWithStyles);
+describe('BasicQueryBuilder -> Boolean Criterion', () => {
+  test('should render', () => {
+    const section = initialSections.getIn(['result', 'children', 'success']);
 
-test('Criterion should render', () => {
-  const section = initialSections.getIn(['result', 'children', 'success']);
+    const criterion = renderer
+      .create(<Criterion section={section} />)
+      .toJSON();
 
-  const criterion = ReactTestRenderer.create(
-    <WrappedCriterion
-      section={section} />
-  ).toJSON();
-
-  expect(criterion).toMatchSnapshot();
-});
-
-test('Criterion with value', () => {
-  const section = initialSections.getIn(['result', 'children', 'success']);
-
-  const mockCriteria = new Map({
-    'statement.result.success': true
+    expect(criterion).toMatchSnapshot();
   });
 
-  const criterion = ReactTestRenderer.create(
-    <WrappedCriterion
-      criterion={mockCriteria}
-      section={section} />
-  ).toJSON();
+  test('with value', () => {
+    const section = initialSections.getIn(['result', 'children', 'success']);
 
-  expect(criterion).toMatchSnapshot();
-});
+    const mockCriteria = new Map({
+      'statement.result.success': true
+    });
 
-test('Criterion value change', () => {
-  const onCriterionChange = jest.fn().mockReturnValueOnce(true);
+    const criterion = renderer
+      .create(<Criterion criterion={mockCriteria} section={section} />)
+      .toJSON();
 
-  const testCriterion = new Criterion({
-    criterion: new Map({
-      $comment: 'A comment',
-    }),
-    section: initialSections.getIn(['result', 'children', 'success']),
-    onCriterionChange,
-    onDeleteCriterion: jest.fn()
+    expect(criterion).toMatchSnapshot();
   });
 
+  test('value change', () => {
+    const onCriterionChange = jest.fn().mockReturnValueOnce(true);
 
-  testCriterion.handleValueChange({
-    target: {
-      value: 'true'
-    }
+    const testCriterion = new Criterion({
+      criterion: new Map({
+        $comment: 'A comment',
+      }),
+      section: initialSections.getIn(['result', 'children', 'success']),
+      onCriterionChange,
+      onDeleteCriterion: jest.fn()
+    });
+
+
+    testCriterion.handleValueChange({
+      target: {
+        value: 'true'
+      }
+    });
+
+    expect(onCriterionChange.mock.calls.length).toEqual(1);
+    expect(onCriterionChange.mock.calls[0][0].get('statement.result.success')).toEqual(true);
   });
-
-  expect(onCriterionChange.mock.calls.length).toEqual(1);
-  expect(onCriterionChange.mock.calls[0][0].get('statement.result.success')).toEqual(true);
 });
