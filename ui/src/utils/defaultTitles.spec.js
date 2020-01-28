37a189c9c304ae2eb887b066d6aa96636c598fd1
Vadim Nikolaenko <vadimnikolaenko.biz@gmail.com>
2020-01-28 16:13:36 +0200

    test(defaultTitles): Fix tests

--- ui/src/utils/defaultTitles.spec.js
+++ ui/src/utils/defaultTitles.spec.js
@@ -1,23 +1,26 @@
-import React from 'react';
 import 'jest-enzyme';
 import { shallow } from 'enzyme';
 import { Map } from 'immutable';
 import { createDefaultTitleWithIcon, getPercentage } from './defaultTitles';
 import chevronDownIcon from './assets/ll-chevron-down-icon.svg';
 
-test('defaultTitles should create a title with an icon', () => {
-  const model = new Map({
-    _id: '1111',
-    description: 'dave',
+describe('defaultTitles', () => {
+  test('should create a title with an icon', () => {
+    const model = new Map({
+      _id: '1111',
+      description: 'dave',
+    });
+
+    const rendered = shallow(createDefaultTitleWithIcon(model));
+
+    expect(rendered).toMatchSnapshot();
   });
 
-  const rendered = shallow(<div>{createDefaultTitleWithIcon(model)}</div>);
-  expect(rendered).toMatchSnapshot();
-});
+  test('should getPercentage', () => {
+    const result = getPercentage(50, 100);
 
-test('defaultTitle should getPercentage', () => {
-  const result = getPercentage(50, 100);
+    expect(result.result).toEqual('100%');
 
-  expect(result.result).toEqual('100%');
-  expect(result.icon).toBe(chevronDownIcon);
+    expect(result.icon).toBe(chevronDownIcon);
+  });
 });
