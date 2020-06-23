c1240d368aad8f22876609bf328b8d6f108e04e1
Lydia Ross <lydia.ross@learningpool.com>
2020-06-23 12:42:56 +0100

    fix(): Remove one createIndex call in migration

--- cli/src/commands/v2-migrations/20171122100800_common_indexes.js
+++ cli/src/commands/v2-migrations/20171122100800_common_indexes.js
@@ -29,10 +29,9 @@ const createStatementIndexes = (connection) => {
   createIndexWithVoid('verbId_objId', { ...verbIdIndex, ...objectIdIndex });
   createIndexWithVoid('mbox', { 'statement.actor.mbox': 1 });
   createIndexWithVoid('account', accountIndex);
-  createIndexWithVoid('timestamp__id', { timestamp: -1, _id: -1 });
   createIndexWithVoid('stored__id', { stored: -1, _id: -1 });
   createIndex(stmts, 'statementId_lrs_id', { 'statement.id': 1, lrs_id: 1 });
-  createIndexWithOrg('timestamp__id', { timestamp: -1, _id: 1 });
+  createIndexWithOrg('timestamp__id', { timestamp: -1, _id: -1 });
   createIndexWithOrg('stored__id', { stored: -1, _id: 1 });
   createIndexWithOrg('objId', objectIdIndex);
   createIndexWithOrg('verbId_objId', { ...verbIdIndex, ...objectIdIndex });
